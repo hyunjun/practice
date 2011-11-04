f717ff933057c042003e6e824e91247e25a8952e
hchung@hchung-desktop <hchung@knoa.com>
2011-11-04 16:28:20 -0400

    commit changes related to maven

--- java/junit-test/src/main/java/com/knoa/practice/App.java
+++ java/junit-test/src/main/java/com/knoa/practice/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.print("Hello World!");
+		for ( String arg : args )
+			System.out.print("\t" + arg);
+        System.out.println();
     }
 }
