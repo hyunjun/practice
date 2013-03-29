900afd1aad25c300348b8340bd93a260bd7be9a4
vagrant <vagrant@precise64.(none)>
2013-03-29 02:17:21 +0000

    ch08 test_twill_browser.py works

--- python/PacktPub.Python.Testing.Beginners.Guide/ch08/test_twill_browser.py
+++ python/PacktPub.Python.Testing.Beginners.Guide/ch08/test_twill_browser.py
@@ -1,26 +1,17 @@
 from unittest import TestCase
 import twill
 
-'''
-import os
-import sys
-sys.path.append(os.path.abspath('/usr/local/etc/twill-0.9/twill')
-'''
-
 class test_twill_browser(TestCase):
-	def setUp(self):
-		import os
-		import sys
-		sys.path.append(os.path.abspath('/usr/local/etc/twill-0.9/twill')
 
 	def test_slashdot(self):
 		browser = twill.get_browser()
+
 		browser.go('http://slashdot.org/')
 		self.assertTrue(browser.get_code() in (200, 201))
-		html = browser.get_html()
-		self.assertTrue(html.count('slashdot') > 150)
+
 		link = browser.find_link('Science')
 		browser.follow_link(link)
+
 		form = browser.get_form(2)
 		form.set_value('aardvark', name='fhfilter')
 		browser.clicked(form, None)
