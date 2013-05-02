b8061bdb984bbacad5e1b36fde03f228e59fbc8f
hyunjun chung <jun@jun-vm-crunchbang.(none)>
2013-05-02 23:16:05 +0900

    complete ML/ex1

--- ML/ex1/gradientDescentMulti.m
+++ ML/ex1/gradientDescentMulti.m
@@ -17,30 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-	n = length(X(1,:))	%	# of features
-	tmpX = [ones(n,1), X]
-	tmp = zeros(n+1, 1)
-	for i = 1:n+1
-		%tmp(i,1) = tmp(i,1) + ((theta(i,1) + theta(i,1) * X(i,2)) - y(i)) * X(i,2);
-		tmp(i,1) = tmp(i,1) + ((theta(i,1) + X(i,2) * theta') - y(i)) * X(i,2);
-	end
-	for i = 1:n+1
-		theta(i,1) = tmp(i,1)
-	end
-
-	%tmp_j1 = 0;
-	%for i = 1:m
-	%	tmp_j1 = tmp_j1 + ((theta(1,1) + theta(2,1) * X(i,2)) - y(i));
-	%end
-	%tmp_j2 = 0;
-	%for i = 1:m
-	%	tmp_j2 = tmp_j2 + ((theta(1,1) + theta(2,1) * X(i,2)) - y(i)) * X(i,2);
-	%end
-	%tmp1 = theta(1,1) - (alpha * ((1/m) * tmp_j1))
-	%tmp2 = theta(2,1) - (alpha * ((1/m) * tmp_j2))
-
-	%theta(1,1) = tmp1
-	%theta(2,1) = tmp2
+	gradJ = 1/(2*m) * 2 * (X'*X*theta - X'*y);
+	theta = theta - alpha * gradJ
 
     % ============================================================
 
