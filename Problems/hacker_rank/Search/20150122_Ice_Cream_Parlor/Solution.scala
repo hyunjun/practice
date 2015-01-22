b2e5ceca1de543bfe36ad5b5522fc085ecd23dcd
Hyunjun Chung <hyunjun@daumcorp.com>
2015-01-22 13:26:35 +0900

    other ways to treat nested iteration in scala

--- Problems/hacker_rank/Search/20150122_Ice_Cream_Parlor/Solution.scala
+++ Problems/hacker_rank/Search/20150122_Ice_Cream_Parlor/Solution.scala
@@ -6,14 +6,18 @@ object Solution {
       val m = inps(i).toInt
       val n = inps(i + 1).toInt
       val flavors = inps(i + 2).split(" ").map((s: String) => s.toInt).toArray
-      for ( a <- 0 until flavors.length - 1 ) {
+      /*for ( a <- 0 until flavors.length - 1 ) {
         for ( b <- a + 1 until flavors.length ) {
           if ( flavors(a) + flavors(b) == m ) {
             println(s"${a + 1} ${b + 1}")
           }
         }
-      }
-      //(0 until flavors.length - 1).flatMap(i => (i + 1 until flavors.length).filter(j => flavors(i) + flavors(j) == m))
+      }*/
+      /*for ( a <- 0 until flavors.length - 1;
+            b <- a + 1 until flavors.length;
+            if flavors(a) + flavors(b) == m )
+        println(s"${a + 1} ${b + 1}")*/
+      (0 until flavors.length - 1).flatMap(a => (a + 1 until flavors.length).filter(flavors(a) + flavors(_) == m).map((a, _))).foreach(r => println(s"${r._1 + 1} ${r._2 + 1}"))
     }
   }
 }
