7fbf6367c37eedbb10679b75cc14341128629a0c
morpheus.0 <morpheus.0@daumkakao.com>
2015-03-05 13:58:01 +0900

    test of isinstance

---
+++ python/test_isinstance.py
@@ -0,0 +1,8 @@
+s = 'string'
+print '{}\t{}'.format(s, isinstance(s, str))
+us = u'unicode'
+print '{}\t{}'.format(us, isinstance(us, unicode))
+num = 3
+print '{}\t{}'.format(num, isinstance(num, int))
+floating_num = 3.0
+print '{}\t{}'.format(floating_num, isinstance(floating_num, float))
