b71f010605383f73786e02ec4ae9acb0ed3f5b54
morpheus.0 <morpheus.0@daumkakao.com>
2015-03-05 14:05:14 +0900

    add collection types

--- python/test_isinstance.py
+++ python/test_isinstance.py
@@ -6,3 +6,12 @@ num = 3
 print '{}\t{}'.format(num, isinstance(num, int))
 floating_num = 3.0
 print '{}\t{}'.format(floating_num, isinstance(floating_num, float))
+
+# https://docs.python.org/2/library/collections.html
+import collections
+l = [1, 2, 3]
+print '{}\t{}'.format(l, isinstance(l, collections.MutableSequence))
+s = {1, 1, 2, 3}
+print '{}\t{}'.format(s, isinstance(s, collections.MutableSet))
+d = {'a':'1', 'b':'2', 'c':'3'}
+print '{}\t{}'.format(d, isinstance(d, collections.MutableMapping))
