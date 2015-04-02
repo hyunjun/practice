8ef91bc4510762c57b02ebfc2e2dc3724af93295
Hyunjun Chung <hyunjun@daumcorp.com>
2015-04-02 15:51:42 +0900

    add more example code

--- java/test-jsonpath/src/test/java/test/json/JsonPathTest.java
+++ java/test-jsonpath/src/test/java/test/json/JsonPathTest.java
@@ -6,7 +6,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.*;
 
 //	https://github.com/jayway/JsonPath
 public class JsonPathTest extends TestCase  {
@@ -16,5 +16,24 @@ public class JsonPathTest extends TestCase  {
     List<String> authors = JsonPath.read(jsonStr, "$.store.book[*].author");
     assertEquals("Nigel Rees", authors.get(0));
     assertEquals("Evelyn Waugh", authors.get(1));
+  }
+
+  class Test  {
+    public final int t;
+    Test(int t) { this.t = t; }
+  }
+
+  public void testArray() {
+    final String jsonStr = "[{'test':0}, {'test':1}, {'test':2}]";
+    System.out.println(JsonPath.parse(jsonStr).read("$[1].test"));
+
+    Object document = Configuration.defaultConfiguration().jsonProvider().parse(jsonStr);
+    System.out.println(JsonPath.read(document, "$[0].test"));
+    System.out.println(JsonPath.read(document, "$[1].test"));
+    System.out.println(JsonPath.read(document, "$[2].test"));
+
+    List<Object> tests = JsonPath.read(document, "$[*]");
+    for ( Object t : tests )
+      System.out.println(JsonPath.read(t, "$.test"));
   }
 }
