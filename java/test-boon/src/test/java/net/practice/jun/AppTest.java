b00e6a1d4b2fba530543935abd564a52ee98719f
Hyunjun Chung <hyunjun@daumcorp.com>
2015-04-28 17:08:12 +0900

    add more example

--- java/test-boon/src/test/java/net/practice/jun/AppTest.java
+++ java/test-boon/src/test/java/net/practice/jun/AppTest.java
@@ -12,9 +12,6 @@ import java.util.List;
 import static org.boon.json.JsonFactory.fromJson;
 import static org.boon.json.JsonFactory.fromJsonArray;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest extends TestCase {
 
   public static Class<MyJson> myJson = MyJson.class;
@@ -36,5 +33,53 @@ public class AppTest extends TestCase {
     List<MyJson> mjs = fromJsonArray(jsonStrs, myJson);
     assertEquals(mjs.get(2).num, 20);
     assertEquals(mjs.get(2).str, "str20");
+  }
+
+  public static Class<Input> classInput = Input.class;
+  public class Input {
+    public WAtt w_att;
+    public String input;
+    public List<Term> terms;
+
+    public final class WAtt {
+      public int pos;
+      public int offset;
+    }
+
+    public final class Term {
+      public String type;
+      public String str;
+      public List<Term> terms;
+      public Syn syn;
+
+      public final class Syn {
+        public String m_syn;
+        public List<String> terms;
+        public List<String> s_syn;
+      }
+    }
+  }
+
+  public void testBoon2() {
+    final String jsonStrs = "[{\"w_att\":{\"pos\":0,\"offset\":0},\"input\":\"ES검색은\",\"terms\":[{\"type\":\"NE\",\"str\":\"다음\",\"syn\":{\"m_syn\":\"ES\",\"terms\":[\"ES\"],\"s_syn\":[\"daum\"]}},{\"type\":\"N\",\"str\":\"검색\"},{\"type\":\"SU\",\"str\":\"검색은\"}]},{\"w_att\":{\"pos\":6,\"offset\":16},\"input\":\"정확하고\",\"terms\":[{\"type\":\"RO\",\"str\":\"정확하다\"},{\"type\":\"SU\",\"str\":\"정확하고\"}]},{\"w_att\":{\"pos\":11,\"offset\":29},\"input\":\"빠르다\",\"terms\":[{\"type\":\"RO\",\"str\":\"빠르다\"},{\"type\":\"SU\",\"str\":\"빠르다\"}]}]";
+    List<Input> inputs = fromJsonArray(jsonStrs, classInput);
+    assertEquals(inputs.get(0).w_att.pos, 0);
+    assertEquals(inputs.get(0).w_att.offset, 0);
+    assertEquals(inputs.get(0).input, "ES검색은");
+    assertEquals(inputs.get(0).terms.get(0).type, "NE");
+    assertEquals(inputs.get(0).terms.get(0).str, "다음");
+    assertEquals(inputs.get(0).terms.get(0).syn.m_syn, "ES");
+    assertEquals(inputs.get(0).terms.get(0).syn.terms.get(0), "ES");
+    assertEquals(inputs.get(0).terms.get(0).syn.s_syn.get(0), "daum");
+    assertEquals(inputs.get(0).terms.get(1).type, "N");
+    assertEquals(inputs.get(0).terms.get(1).str, "검색");
+    assertEquals(inputs.get(0).terms.get(2).type, "SU");
+    assertEquals(inputs.get(0).terms.get(2).str, "검색은");
+    assertEquals(inputs.get(1).w_att.pos, 6);
+    assertEquals(inputs.get(1).w_att.offset, 16);
+    assertEquals(inputs.get(1).input, "정확하고");
+    assertEquals(inputs.get(2).w_att.pos, 11);
+    assertEquals(inputs.get(2).w_att.offset, 29);
+    assertEquals(inputs.get(2).input, "빠르다");
   }
 }
