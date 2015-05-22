f9f6362ddb8685cb745f9f622b06a322b8b964d4
Hyunjun Chung <hyunjun@daumcorp.com>
2015-05-22 09:30:11 +0900

    https://www.hackerrank.com/challenges/java-end-of-file/ https://www.hackerrank.com/challenges/java-end-of-file/submissions/code/11721473

---
+++ Problems/hacker_rank/Java/Introduction/End_of_file/EndOfFile.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EndOfFile {
+  public static void main(final String[] args)  {
+    Scanner sc = new Scanner(System.in);
+    int i = 1;
+    while ( sc.hasNext() )  {
+      System.out.println(i++ + " " + sc.nextLine());
+    }
+  }
+}
