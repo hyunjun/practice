8149fb3fc8dc4cbfe2fdae53772c39ddc9bf97fc
Hyunjun Chung <hyunjun@daumcorp.com>
2015-05-29 09:18:26 +0900

    https://www.hackerrank.com/challenges/java-hashset https://www.hackerrank.com/challenges/java-hashset/submissions/code/11807379

---
+++ Problems/hacker_rank/Java/DataStructures/JavaHashset/Solution.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+  public static void main(String[] args)  {
+    Scanner sc = new Scanner(System.in);
+    int T = Integer.parseInt(sc.nextLine());
+    Set<String> s = new HashSet<String>();
+    for ( int i = 0; i < T; ++i ) {
+      s.add(sc.nextLine());
+      System.out.println(s.size());
+    }
+  }
+} 
