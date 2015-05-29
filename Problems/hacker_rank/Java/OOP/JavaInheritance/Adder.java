a05b6f403c688bdd64ba5dc42c1c825e04c1bb76
Hyunjun Chung <hyunjun@daumcorp.com>
2015-05-29 09:29:31 +0900

    https://www.hackerrank.com/challenges/java-inheritence https://www.hackerrank.com/challenges/java-inheritence/submissions/code/11807415

---
+++ Problems/hacker_rank/Java/OOP/JavaInheritance/Adder.java
@@ -0,0 +1,14 @@
+class Arithmetic  {
+  public int add(int x, int y) { return x + y; }
+}
+
+class Adder extends Arithmetic  {
+}
+
+class Solution{
+  public static void main(String []argh){
+    Adder X=new Adder();
+    System.out.println("My superclass is: "+X.getClass().getSuperclass().getName());    
+    System.out.print(X.add(10,32)+" "+X.add(10,3)+" "+X.add(10,10)+"\n");
+  }
+}
