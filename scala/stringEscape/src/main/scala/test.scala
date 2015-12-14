249ea9eca1c7ff81e58aff9527028b02f7dd2575
hyunjun <agapelover4u@yahoo.co.kr>
2015-12-14 13:46:18 +0900

    Implement escapeJson just replacing chars

--- scala/stringEscape/src/main/scala/test.scala
+++ scala/stringEscape/src/main/scala/test.scala
@@ -4,9 +4,14 @@ object Test {
 
   //  http://stackoverflow.com/questions/9913971/scala-how-can-i-get-an-escaped-representation-of-a-string
   //  http://stackoverflow.com/questions/25189608/cant-import-scala-reflect-runtime-universe
-  def escape(raw: String): String = {
+  /*def escape(raw: String): String = {
     import scala.reflect.runtime.universe._
     Literal(Constant(raw)).toString
+  }*/
+  //  http://www.ietf.org/rfc/rfc4627.txt
+  //  https://commons.apache.org/proper/commons-lang/apidocs/src-html/org/apache/commons/lang3/StringEscapeUtils.html
+  def escapeJson(raw: String): String = {
+    raw.replace("\\", "\\\\").replace("\"", "\\\"").replace("/", "\\/")
   }
 
   def main(args: Array[String]) {
@@ -15,14 +20,20 @@ object Test {
     println(StringEscapeUtils.escapeJson(s))
     val s1 = "이후 최택의 방으로 들어온 김선우는 \"사랑하는 친구야\"라며 비밀로 해달라고 부탁했다."
     println(s1)
-    println(StringEscapeUtils.escapeCsv(s1))
+    /*println(StringEscapeUtils.escapeCsv(s1))
     println(StringEscapeUtils.escapeEcmaScript(s1))
     println(StringEscapeUtils.escapeHtml3(s1))
     println(StringEscapeUtils.escapeHtml4(s1))
     println(StringEscapeUtils.escapeJava(s1))
     println(StringEscapeUtils.escapeJson(s1))
     println(StringEscapeUtils.escapeXml10(s1))
-    println(StringEscapeUtils.escapeXml11(s1))
-    println(escape(s1))
+    println(StringEscapeUtils.escapeXml11(s1))*/
+    println(StringEscapeUtils.escapeJson(s1))
+    println(new String(StringEscapeUtils.escapeJson(s1).toString.getBytes("UTF-8"), "UTF-8"))
+    println(escapeJson(s1))
+    val s2 = "('"
+    println(s2)
+    println(StringEscapeUtils.escapeJson(s2))
+    println(escapeJson(s2))
   }
 }
