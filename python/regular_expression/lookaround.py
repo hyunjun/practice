f1d6953d771144bb7776be2d30257249c4279fd2
hyunjun <agapelover4u@yahoo.co.kr>
2016-06-09 11:09:30 +0900

    add contents from http://www.ohmynews.com/NWS_Web/View/at_pg.aspx?CNTN_CD=A0002216442

---
+++ python/regular_expression/lookaround.py
@@ -0,0 +1,16 @@
+import re
+
+# http://www.ohmynews.com/NWS_Web/View/at_pg.aspx?CNTN_CD=A0002216442
+
+sentences = ['The history of apple computer', 'News for apple computer', 'The apple is the pomaceous fruit of the apple tree', 'apple computer is not the apple']
+
+pattern_words = ['apple', 'apple (?!computer)', 'apple (?=computer)', '(?<=apple) computer']
+
+for pattern_word in pattern_words:
+  print 'pattern: {}'.format(pattern_word)
+  pattern = re.compile(pattern_word)
+  for sentence in sentences:
+    print '\t"{}" matched ['.format(sentence),
+    for m in re.finditer(pattern, sentence):
+      print m.group(),
+    print ']'
