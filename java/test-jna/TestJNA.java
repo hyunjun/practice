5c2de8e1f208d9b0596bd45eef91e58ad5a033f4
morpheus.0 <morpheus.0@kakaocorp.com>
2016-11-24 10:48:27 +0900

    Add more test functions which return primitive types such as int, float

--- java/test-jna/TestJNA.java
+++ java/test-jna/TestJNA.java
@@ -4,10 +4,18 @@ import com.sun.jna.Native;
 public class TestJNA  {
   public interface CLibrary extends Library {
     public void exampleMethod(String val);
+    public int example_of_add_strlen(String s1, String s2);
+    public int example_of_int(int a, int b);
+    public float example_of_float(float a, float b);
   }
 
   public static void main(String[] args)  {
     CLibrary mylib = (CLibrary)Native.loadLibrary("test", CLibrary.class);
     mylib.exampleMethod("ImAString");
+    System.out.println(mylib.example_of_add_strlen("str1", "string2"));
+    System.out.println(mylib.example_of_add_strlen("", "string2"));
+    System.out.println(mylib.example_of_add_strlen("str1", null));
+    System.out.println(mylib.example_of_int(2, 3));
+    System.out.println(mylib.example_of_float((float) 2.1, (float) 3.2));
   }
 }
--- java/test-jna/test.c
+++ java/test-jna/test.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 void exampleMethod(const char* value) {
   printf("%s\n", value);
+}
+
+int example_of_add_strlen(const char* s1, const char* s2)  {
+  int l1 = 0;
+  int l2 = 0;
+  if ( s1 != NULL ) {
+    l1 = strlen(s1);
+  }
+  if ( s2 != NULL ) {
+    l2 = strlen(s2);
+  }
+  return l1 + l2;
+}
+
+int example_of_int(int a, int b)  {
+  return a + b;
+}
+
+float example_of_float(float a, float b)  {
+  return a + b;
 }
