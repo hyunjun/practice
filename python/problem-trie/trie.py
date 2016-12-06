2f6e656e45268be0035e6ecc5b61575340d9d3f3
morpheus.0 <morpheus.0@kakaocorp.com>
2016-12-06 16:32:09 +0900

    very simple trie using dict

---
+++ python/problem-trie/trie.py
@@ -0,0 +1,26 @@
+#   http://www.geeksforgeeks.org/trie-insert-and-search/
+
+def str2trie(strs):
+    root = {}
+    for s in strs:
+        cur = root
+        for ch in s:
+            if ch not in cur:
+                cur[ch] = {}
+            cur = cur[ch]
+    return root
+
+
+def search(root, s):
+    cur = root
+    for ch in s:
+        if ch not in cur:
+            return False
+        cur = cur[ch]
+    return True
+
+strs = ['bye', 'any', 'answer', 'there', 'their']
+trie_root = str2trie(strs)
+for s in strs:
+    print s, search(trie_root, s)
+print 'and', search(trie_root, 'and')
