9cd5e1412aeefa35b5ad9795968cd6c0f6275669
morpheus.0 <morpheus.0@kakaocorp.com>
2017-02-01 13:37:33 +0900

    permutation by recursive call

---
+++ python/problem-permutation/permutation2.py
@@ -0,0 +1,16 @@
+#   similar version in scala; https://gist.github.com/hyunjun/ccf149468a9d87857d0ce673e0127eb1
+def permutation(acc, prev, cur):
+    if 0 == len(cur):
+        return [prev]
+    else:
+        result = []
+        [result.extend(permutation(acc, prev + cur[i], cur[:i] + cur[i + 1:])) for i in range(0, len(cur))]
+        return result
+
+acc = []
+acc.extend(permutation(acc, "", "aet"))
+print acc
+
+acc = []
+acc.extend(permutation(acc, "", "aaet"))
+print acc
