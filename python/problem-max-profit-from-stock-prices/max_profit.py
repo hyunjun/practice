390d99984226f23142cf6c06946a5af1b7269850
hyunjun <agapelover4u@yahoo.co.kr>
2017-02-07 01:18:40 +0900

    problems from www.interviewcake.com

---
+++ python/problem-max-profit-from-stock-prices/max_profit.py
@@ -0,0 +1,15 @@
+# https://www.interviewcake.com/question/python/stock-price
+
+def max_profit(arr):
+  min_val = arr[0]
+  max_profit = 0
+  for i in range(1, len(arr)):
+    cur_profit = arr[i] - min_val
+    if cur_profit < 0:
+      min_val = arr[i]
+    else:
+      if max_profit < cur_profit:
+        max_profit = cur_profit
+  return max_profit
+
+print max_profit([10, 7, 5, 8, 11, 9])
---
+++ python/problem-product-of-list-except-the-index/product.py
@@ -0,0 +1,15 @@
+# https://www.interviewcake.com/question/python/product-of-other-numbers
+
+def product(arr):
+  result = [1] * len(arr)
+  acc = 1
+  for i in range(1, len(arr)):
+    acc *= arr[i - 1]
+    result[i] *= acc 
+  acc = 1
+  for i in range(len(arr) - 2, -1, -1):
+    acc *= arr[i + 1]
+    result[i] *= acc
+  return result
+
+print product([1, 7, 3, 4])
