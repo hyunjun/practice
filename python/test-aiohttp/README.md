533b0c1c9d4c129ec515107c74ab4c8fe9f2fc61
hyunjun <agapelover4u@yahoo.co.kr>
2017-08-07 02:11:03 +0900

    [aiohttp] basic url request

---
+++ python/test-aiohttp/README.md
@@ -0,0 +1,10 @@
+aiohttp
+=======
+
+* installation `pip3 install aiohttp`
+* ref
+  * [aiohttp: Asynchronous HTTP Client/Server](http://aiohttp.readthedocs.io/)
+  * [AsyncIO for the Working Python Developer](https://hackernoon.com/asyncio-for-the-working-python-developer-5c468e6e2e8e)
+  * [Aiohttp로 대량의 requests 처리하기](https://maxtortime.me/aiohttp-requests/)
+  * [python async URL요청](https://dreamtamercom.wordpress.com/2017/03/22/python-async-url%EC%9A%94%EC%B2%AD/)
+  * [Exploring Python 3’s Asyncio by Example](http://www.giantflyingsaucer.com/blog/?p=5557)
---
+++ python/test-aiohttp/test_aiohttp_urls.py
@@ -0,0 +1,50 @@
+from concurrent.futures import ALL_COMPLETED
+import aiohttp
+import asyncio
+import logging
+import sys
+
+
+async def url2json3(logger, url):
+  try:
+    response = await aiohttp.request('GET', url)
+  except:
+    return '{} unresponsive'.format(url)
+  assert response.status == 200
+  # result = await response.json()  # for json 
+  result = await response.text()
+  response.close()
+  return result
+
+
+async def get_system_tagged3(logger, urls):
+  futures = [url2json3(logger, url) for url in urls]
+  done, pending = await asyncio.wait(futures, timeout=1, return_when=ALL_COMPLETED)
+  for future in pending:
+    future.cancel()
+  responses = []
+  for future in done:
+    responses.append(future.result())
+  return responses
+
+
+if __name__ == '__main__':
+  logger = logging.getLogger()
+  logger.setLevel(logging.DEBUG)
+  ch = logging.StreamHandler(sys.stdout)
+  ch = logging.NullHandler()
+  logger.addHandler(ch)
+  
+  urls = ['http://daum.net', 'http://naver.com', 'http://google.com'] 
+
+  result = []
+  event_loop = asyncio.get_event_loop()
+  try:
+    result = event_loop.run_until_complete(get_system_tagged3(logger, urls))
+  except:
+    event_loop.close()
+  for url, r in zip(urls, result):
+    print('=' * 50)
+    print(url)
+    print('-' * 50)
+    print(r)
