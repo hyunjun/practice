aa5d8a191379f8d4c79b8a8a60081cc2987408c7
morpheus.0 <morpheus.0@kakaocorp.com>
2017-11-21 09:46:13 +0900

    problem for lower & upper bound

--- python/problem-binary-search-lower-bound/lower_bound.py
+++ python/problem-binary-search-lower-bound/lower_bound.py
@@ -2,7 +2,7 @@ def lower_bound(nums, target):
     l, r = 0, len(nums) - 1
     if target == nums[l]:
         return l
-    if target == nums[r]:
+    if nums[r] <= target:
         return r
     while l < r:
         m = (int)((l + r) / 2)
@@ -19,9 +19,37 @@ def lower_bound(nums, target):
     return -1
 
 
+def upper_bound(nums, target):
+    l, r = 0, len(nums) - 1
+    if target <= nums[l]:
+        return l
+    if target == nums[r]:
+        return r
+    while l < r:
+        m = (int)((l + r) / 2)
+        if nums[m] == target:
+            return m
+        elif nums[m] < target:
+            l = m + 1
+            if target <= nums[l]:
+                return l
+        else:   #   target < nums[m]
+            r = m - 1
+            if nums[r] < target:
+                return m
+    return -1
+
+
 nums = [-5, -1, 1, 4]
-cases = [(-6, -1), (-5, 0), (-3, 0), (-1, 1), (0, 1), (1, 2), (2, 2), (4, 3), (5, -1)]
+
+lower_bound_cases = [(-6, -1), (-5, 0), (-3, 0), (-1, 1), (0, 1), (1, 2), (2, 2), (4, 3), (5, 3)]
 print('nums {}'.format(nums))
-for target, expected in cases:
+for target, expected in lower_bound_cases:
     real = lower_bound(nums, target)
+    print('target {}\texpected {}\treal {}\tresult {}'.format(target, expected, real, expected == real))
+
+upper_bound_cases = [(-6, 0), (-5, 0), (-3, 1), (-1, 1), (0, 2), (1, 2), (2, 3), (4, 3), (5, -1)]
+print('\nnums {}'.format(nums))
+for target, expected in upper_bound_cases:
+    real = upper_bound(nums, target)
     print('target {}\texpected {}\treal {}\tresult {}'.format(target, expected, real, expected == real))
