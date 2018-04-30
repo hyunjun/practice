37ab0f50637460db79ee45410a71aeaa69f8a16d
hyunjun <agapelover4u@yahoo.co.kr>
2018-05-01 00:01:59 +0900

    [Easy] problem for https://leetcode.com/problems/remove-duplicates-from-sorted-list

---
+++ python/problem-linked-list/remove_duplicates_from_sorted_list.py
@@ -0,0 +1,44 @@
+#   https://leetcode.com/problems/remove-duplicates-from-sorted-list
+#   100.00%
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+    def __str__(self):
+        return "{}->{}".format(self.val, self.next)
+
+
+class Solution:
+    def deleteDuplicates(self, head):
+        if head is None:
+            return None
+        prev = head
+        cur = prev.next
+        while prev is not None and cur is not None:
+            if prev.val == cur.val:
+                prev.next = cur.next
+            else:
+                prev = cur
+            cur = cur.next
+        return head
+
+
+s = Solution()
+
+l1 = ListNode(1)
+l1.next = ListNode(1)
+l1.next.next = ListNode(2)
+print(l1)
+head = s.deleteDuplicates(l1)
+print(head)
+
+l2 = ListNode(1)
+l2.next = ListNode(1)
+l2.next.next = ListNode(2)
+l2.next.next.next = ListNode(3)
+l2.next.next.next.next = ListNode(3)
+print(l2)
+head = s.deleteDuplicates(l2)
+print(head)
