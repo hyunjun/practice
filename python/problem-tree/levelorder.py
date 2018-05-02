b56b1bbc097e393a6afc60341995a4f913ac017c
hyunjun <agapelover4u@yahoo.co.kr>
2018-05-02 22:26:08 +0900

    Add tree traversals

--- python/problem-tree/levelorder.py
+++ python/problem-tree/levelorder.py
@@ -1,26 +1,19 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+from inorder import TreeNode
+from inorder import root
 
-root = Node(6)
-root.left = Node(3)
-root.right = Node(8)
-root.left.left = Node(2)
-root.left.right = Node(4)
-root.left.left.left = Node(1)
-root.left.right.right = Node(5)
-root.right.right = Node(9)
 
-queue = []
-cur = root
-queue.append(cur)
-while 0 < len(queue):
-    cur = queue[0]
-    print cur.data
-    queue.remove(cur)
-    if cur.left is not None:
-        queue.append(cur.left)
-    if cur.right is not None:
-        queue.append(cur.right)
+def levelorder(node):
+    queue, res = [node], []
+    while queue:
+        cur = queue[0]
+        del queue[0]
+        res.append(cur.val)
+        if cur.left:
+            queue.append(cur.left)
+        if cur.right:
+            queue.append(cur.right)
+    return res
+
+
+if __name__ == '__main__':
+    print(levelorder(root))
--- python/problem-tree/postorder.py
+++ python/problem-tree/postorder.py
@@ -2,19 +2,24 @@ from inorder import TreeNode
 from inorder import root
 
 
-def preorder_recur(node, res):
+def postorder_recur(node, res):
     if node is None:
         return
+    postorder_recur(node.left, res)
+    postorder_recur(node.right, res)
     res.append(node.val)
-    preorder_recur(node.left, res)
-    preorder_recur(node.right, res)
 
 
-def preorder(node):
+def postorder(node):
     stack, res = [node], []
     while stack:
         cur = stack.pop()
-        res.append(cur.val)
+        if cur.visited:
+            if cur.val not in res:
+                res.append(cur.val)
+        else:
+            cur.visited = True
+            stack.append(cur)
         if cur.right:
             stack.append(cur.right)
         if cur.left:
@@ -24,6 +29,6 @@ def preorder(node):
 
 if __name__ == '__main__':
     res = []
-    preorder_recur(root, res)
+    postorder_recur(root, res)
     print(res)
-    print(preorder(root))
+    print(postorder(root))
