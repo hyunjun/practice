c7969ae7093ccf789f128fd65c547bf6f7c09c1c
hyunjun <agapelover4u@yahoo.co.kr>
2018-05-23 23:42:38 +0900

    problme for count of encoded chars

--- python/problem-decode-ways/count_of_decoded_chars.py
+++ python/problem-decode-ways/count_of_decoded_chars.py
@@ -1,3 +1,26 @@
+'''
+cases
+1~9 a~i
+    1~9다음에 숫자가 바로 나온다면 a~i 한 글자
+    숫자 숫자 괄호 아님
+1~9 + (n)   a~i * n
+    1~9다음에 괄호가 나온다면 a~i n 글자
+    숫자 괄호
+10#~26# j~z
+    1~9다음에 숫자# + 숫자가 나온다면 j~z 한 글자
+    숫자 숫자 # 숫자
+10#~26# + (n)   j~z * n
+    1~9다음에 숫자# + 괄호가 나온다면 j~z n 글자
+    숫자 숫자 # 괄호
+
+즉 입력의 현재 index i에서 최소 +1~+3까지 살펴봐야 어느 case인지 확정
+
+일단 바로 종료하는 case를 살펴보면
+1~9
+1~9(n)
+10#~26#
+10#~26#(n)
+'''
 
 
 def count_of_decoded_chars(inp):
@@ -34,36 +57,59 @@ def count_of_decoded_chars(inp):
     return result
 
 
+#inp = '23#(2)24#2(2)25#126#23#(3)'  #   wwxbbyazwww
+#[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 1, 1, 1]
+#print(count_of_decoded_chars(inp))
+#inp = '2'   #   b
+#[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#inp = '2(2)'   #   bb
+#[0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#inp = '10#'   #   j
+#[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#inp = '10#(2)'   #   jj
+#[0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#print(count_of_decoded_chars(inp))
 
 
-'''
-cases
-1~9 a~i
-    1~9다음에 숫자가 바로 나온다면 a~i 한 글자
-    숫자 숫자 괄호 아님
-1~9 + (n)   a~i * n
-    1~9다음에 괄호가 나온다면 a~i n 글자
-    숫자 괄호
-10#~26# j~z
-    1~9다음에 숫자# + 숫자가 나온다면 j~z 한 글자
-    숫자 숫자 # 숫자
-10#~26# + (n)   j~z * n
-    1~9다음에 숫자# + 괄호가 나온다면 j~z n 글자
-    숫자 숫자 # 괄호
+class Solution:
+    def countOfEncodedChars(self, inp):
+        result = [0] * 26
+        idx = len(inp) - 1
+        while -1 < idx:
+            cnt = 1
+            if ')' == inp[idx]:
+                openIdx = idx - 1
+                while -1 < openIdx and '(' != inp[openIdx]:
+                    openIdx -= 1
+                cnt = int(inp[openIdx + 1:idx])
+                idx = openIdx - 1
+            if '#' == inp[idx]:
+                charIdx = int(inp[idx - 2:idx]) - 1
+                idx -= 3
+            else:
+                charIdx = int(inp[idx]) - 1
+                idx -= 1
+            result[charIdx] += cnt
+        return result
 
-즉 입력의 현재 index i에서 최소 +1~+3까지 살펴봐야 어느 case인지 확정
+    def count2chars(self, inp):
+        return ' '.join(['{}{}'.format(chr(i + 97), n) for i, n in enumerate(inp) if 0 < n])
 
-일단 바로 종료하는 case를 살펴보면
-1~9
-1~9(n)
-10#~26#
-10#~26#(n)
-'''
 
-inp = '23#(2)24#2(2)25#126#23#(3)'  #   wwxbbyazwww
-print(count_of_decoded_chars(inp))
-inp = '2'   #   b
-inp = '2(2)'   #   b
-inp = '10#'   #   b
-inp = '10#(2)'   #   b
-print(count_of_decoded_chars(inp))
+s = Solution()
+        # ww    x  bb  y  az  www
+data = [('23#(2)24#2(2)25#126#23#(3)', [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 1, 1, 1]),
+        # b
+        ('2', [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        # bb
+        ('2(2)', [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        # j
+        ('10#', [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        # jj
+        ('10#(2)', [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        ]
+for inp, expected in data:
+    real = s.countOfEncodedChars(inp)
+    strAndCount = s.count2chars(real)
+    print('{}, {}, result {}'.format(inp, strAndCount, expected == real))
+    print(real)
