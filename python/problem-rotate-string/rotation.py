5663c55037d25bce83f7e4536ad13e42c4d9b27c
morpheus.0 <morpheus.0@kakaocorp.com>
2018-06-18 14:39:30 +0900

    Debug or rotating a character

--- python/problem-rotate-string/rotation.py
+++ python/problem-rotate-string/rotation.py
@@ -5,10 +5,8 @@
 
 class Solution:
     def rotate(self, c, cnt):
-        target_num = ord(c) + cnt
-        if 122 < target_num:
-            target_num -= (ord('z') - ord('a') + 1)
-        return chr(target_num)
+        targetNum = ord(c) + cnt
+        return chr(((targetNum - ord('a')) % 26) + ord('a'))
 
     def rotation(self, inp):
         if inp is None or 0 == len(inp):
@@ -26,9 +24,25 @@ class Solution:
 
         return ''.join(result)
 
+    def rotation1(self, inp):
+        count = [0] * (ord('z') - ord('a') + 1)
+
+        res = []
+        for i in range(len(inp)):
+            targetNum = ord(inp[i]) + count[ord(inp[i]) - ord('a')]
+            res.append(((targetNum - ord('a')) % 26) + ord('a'))
+            count[ord(inp[i]) - ord('a')] += 1
+        return ''.join([chr(r) for r in res])
+
 
 s = Solution()
-data = [('abc', 'abc'), ('aaa', 'abc'), ('apapap', 'apbqcr'), ('zzz', 'zab')]
+data = [('abc', 'abc'), ('aaa', 'abc'), ('apapap', 'apbqcr'), ('zzz', 'zab'),
+        ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrst'),
+        ]
 for inp, expected in data:
     real = s.rotation(inp)
-    print('inp {}, expected {}, real {}, result {}'.format(inp, expected, real, expected == real))
+    real1 = s.rotation1(inp)
+    if len(inp) < 20:
+        print('inp {}, expected {}, real {}, real1 {}, result {}'.format(inp, expected, real, real1, expected == real == real1))
+    else:
+        print('inp      {}\nexpected {}\nreal     {}\nreal 1   {}\nresult {}'.format(inp, expected, real, real1, expected == real == real1))
