23322fec830dda7b2999ac1786a2fb7e67927019
hyunjun <agapelover4u@yahoo.co.kr>
2018-06-19 23:46:33 +0900

    problem for moving zeroes to right

---
+++ python/problem-ETC/move_zeros.py
@@ -0,0 +1,31 @@
+#   move all the zeroes to right
+#   in-place
+
+
+class Solution:
+    def moveZeroes(self, nums):
+        if nums is None or 0 == len(nums):
+            return []
+        l, r = 0, len(nums) - 1
+        while l < r:
+            while l < len(nums) - 1 and nums[l] != 0:
+                l += 1
+            while 0 < r and nums[r] == 0:
+                r -= 1
+            if l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        return nums
+
+
+s = Solution()
+data = [([1, 0, 8, -1, 10, 0], [1, 10, 8, -1, 0, 0]),
+        ([1, 2, 3, 4], [1, 2, 3, 4]),
+        ([0, 1, 2, 3], [3, 1, 2, 0]),
+        ([0, 0, 2, 3], [3, 2, 0, 0]),
+        ]
+for nums, expected in data:
+    print(nums)
+    real = s.moveZeroes(nums)
+    print('\texpected {}, real {}, result {}'.format(nums, expected, real, expected == real))
