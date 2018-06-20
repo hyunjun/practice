6fbd6c25a8958e32d9349650084f8ce39f29afed
hyunjun <agapelover4u@yahoo.co.kr>
2018-06-21 01:16:28 +0900

    Convert python2 files to python3 in test-regex

--- python/test-regex/lookaround.py
+++ python/test-regex/lookaround.py
@@ -7,10 +7,7 @@ sentences = ['The history of apple computer', 'News for apple computer', 'The ap
 pattern_words = ['apple', 'apple (?!computer)', 'apple (?=computer)', '(?<=apple) computer']
 
 for pattern_word in pattern_words:
-  print 'pattern: {}'.format(pattern_word)
+  print('pattern: |{}|'.format(pattern_word))
   pattern = re.compile(pattern_word)
   for sentence in sentences:
-    print '\t"{}" matched ['.format(sentence),
-    for m in re.finditer(pattern, sentence):
-      print m.group(),
-    print ']'
+    print('\t"{}" matched [{}]'.format(sentence, ','.join([m.group() for m in re.finditer(pattern, sentence)])))
