2e2b831e97571570b9c387c52bf0e904799ad738
hyunjun <agapelover4u@yahoo.co.kr>
2018-06-24 01:47:17 +0900

    hackernews.py; python2 to python3

--- python/test-hackernews/hackernews.py
+++ python/test-hackernews/hackernews.py
@@ -1,7 +1,6 @@
-# -*- coding: utf8 -*-
+from urllib.request import urlopen
 import json
 import re
-import urllib
 
 
 NUM_PATTERN = re.compile('\d+')
@@ -17,21 +16,23 @@ def item_url(number):
 
 
 if __name__ == '__main__':
-  max_num = int(urllib.urlopen(MAX_ITEM_URL).read())
-  print max_num
-  item_num_string = ''.join([urllib.urlopen(url).read() for url in [TOPSTORIES_URL, NEWSTORIES_URL]])
-  print item_num_string
+  max_num = int(urlopen(MAX_ITEM_URL).read())
+  print(max_num)
+  item_num_string = ''.join([urlopen(url).read().decode('utf8') for url in [TOPSTORIES_URL, NEWSTORIES_URL]])
+  print(item_num_string)
   item_nums = set([item_num.group() for item_num in re.finditer(NUM_PATTERN, item_num_string)])
   item_urls = [item_url(item_num) for item_num in item_nums]
-  print item_urls
+  print(item_urls)
   dicts = []
   for url in item_urls:
-    print url
-    d = json.loads(urllib.urlopen(url).read())
+    print(url)
+    d = json.loads(urlopen(url).read())
+    if d is None or 'title' not in d or 'url' not in d:
+      continue
     title, url = d['title'], d['url']
     if title is None or url is None or 0 == len(title) or 0 == len(url):
       continue
-    print d['id'], d['score']
+    print(d['id'], d['score'])
     dicts.append(d)
 
   with open('result.md', 'w') as f:
