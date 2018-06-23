b0ebc1e7a1178554fc091243fd47e6f76ac565a6
hyunjun <agapelover4u@yahoo.co.kr>
2018-06-24 01:27:47 +0900

    print_parenthesis.py; python2 to python3

--- python/problem-ETC/print_parenthesis.py
+++ python/problem-ETC/print_parenthesis.py
@@ -4,7 +4,7 @@
 #   Wrong Answer
 def print_parenthesis(n):
     def print_parenthesis_r(s, o, c):   #   s = str, o = # of open, c = # of close
-        #print 'start', s, o, c
+        #print('start', s, o, c)
         if o == 0:
             for i in range(c):
                 s += ')'
@@ -13,11 +13,11 @@ def print_parenthesis(n):
             if 0 < o:
                 print_parenthesis_r(s + '(', o-1, c)
             if 0 < c:
-                #print s + ')', o, c-1
+                #print(s + ')', o, c-1)
                 print_parenthesis_r(s + ')', o, c-1)
         elif s[-1] == ')':
             if 0 < o:
-                #print s + '(', o-1, c
+                #print(s + '(', o-1, c)
                 print_parenthesis_r(s + '(', o-1, c)
     print_parenthesis_r('(', n-1, n)
 
