d25e62adbc469341424d90f3bf867da4fb694049
hyunjun <agapelover4u@yahoo.co.kr>
2018-06-24 01:04:05 +0900

    knapsack.py; python2 to python3

--- python/problem-knapsack/knapsack.py
+++ python/problem-knapsack/knapsack.py
@@ -6,7 +6,7 @@ def maximize_price(price_weight_list, limit_weight):
     def _maximize_price(checked_list, unchecked_list):
         sum_of_price = sum([t[0] for i, t in enumerate(price_weight_list) if i in checked_list])
         sum_of_weight = sum([t[1] for i, t in enumerate(price_weight_list) if i in checked_list])
-        # print checked_list, '->', sum_of_price
+        # print(checked_list, '->', sum_of_price)
 
         if sum_of_weight < limit_weight:
             prices = []
@@ -15,7 +15,7 @@ def maximize_price(price_weight_list, limit_weight):
                 tmp_checked_list.append(i)
                 tmp_unchecked_list = unchecked_list[:]
                 del tmp_unchecked_list[tmp_unchecked_list.index(i)]
-                # print tmp_checked_list, tmp_unchecked_list
+                # print(tmp_checked_list, tmp_unchecked_list)
                 prices.append(_maximize_price(tmp_checked_list, tmp_unchecked_list))
             return max(prices)
 
@@ -24,7 +24,7 @@ def maximize_price(price_weight_list, limit_weight):
 
         return 0
 
-    return _maximize_price([], range(len(price_weight_list)))
+    return _maximize_price([], list(range(len(price_weight_list))))
 
 
 def combination(n):
@@ -54,5 +54,5 @@ def maximize_price2(price_weight_list, limit_weight):
 
 
 if __name__ == '__main__':
-    print maximize_price([(4, 12), (2, 2), (2, 1), (1, 1), (10, 4)], 5)
-    print maximize_price2([(4, 12), (2, 2), (2, 1), (1, 1), (10, 4)], 5)
+    print(maximize_price([(4, 12), (2, 2), (2, 1), (1, 1), (10, 4)], 5))
+    print(maximize_price2([(4, 12), (2, 2), (2, 1), (1, 1), (10, 4)], 5))
