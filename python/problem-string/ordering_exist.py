8135eecc027e3d73be39e0fe88ab49d3ca9a1fdd
hyunjun <agapelover4u@yahoo.co.kr>
2018-06-23 23:13:01 +0900

    move ordering_exist.py to problem-string

--- python/problem-string/ordering_exist.py
+++ python/problem-string/ordering_exist.py
@@ -1,18 +1,20 @@
 # https://www.careercup.com/question?id=5659201272545280
 
-def has_ordering(inp, ord):
+
+def has_ordering(inp, order):
   d = {}
   for i, c in enumerate(inp):
     d[c] = i
-  prev = d[ord[0]]
-  for c in ord:
+  prev = d[order[0]]
+  for c in order:
     cur = d[c]
     if prev > cur:
       return False
     prev = cur
   return True
 
+
 # the first expected result is False because all Ls are not before all Os
 data = [('hello world!', 'hlo!', False), ('hello world!', '!od', False), ('hello world!', 'he!', True), ('aaaabbbbbcccccc', 'ac', True)]
-for inp, ord, expected_result in data:
-  print inp, ord, expected_result, has_ordering(inp, ord)
+for inp, order, expected_result in data:
+  print(inp, order, expected_result, has_ordering(inp, order))
