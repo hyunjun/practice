5d1e7ee7a80f26c9dcc9d21e0380ba838ec6d2f2
hyunjun <agapelover4u@yahoo.co.kr>
2018-06-24 00:39:27 +0900

    max_subarray_xor.py; python2 to python3

--- python/problem-maximum-subarray/max_subarray_xor.py
+++ python/problem-maximum-subarray/max_subarray_xor.py
@@ -1,5 +1,7 @@
 #   http://www.geeksforgeeks.org/find-the-maximum-subarray-xor-in-a-given-array/
 from copy import deepcopy
+from functools import reduce
+
 
 def subset(arr):
     if 0 == len(arr):
@@ -15,23 +17,23 @@ def subset(arr):
 
 def max_subarray_xor(arr):
     candidates, max_val = [], 0
-    for indices in subset(range(len(arr))):
+    for indices in subset(list(range(len(arr)))):
         candidate_arr = [arr[idx] for idx in indices]
         if 0 == len(candidate_arr):
             continue
         val = reduce(lambda a, b: a ^ b, candidate_arr)
-        # print candidate_arr, val, max_val, candidates
+        # print(candidate_arr, val, max_val, candidates)
         if max_val < val:
             candidates = [candidate_arr]
             max_val = val
         elif max_val == val:
             candidates.append(candidate_arr)
-    # print candidates
-    return sorted(candidates, key=lambda t: len(t), reverse=True)[0]
+    #print(candidates)
+    return sorted(candidates, key=lambda t: len(t))[0]
+
 
 data = [([1, 2, 3, 4], 7),
         ([8, 1, 2, 12, 7, 6], 15),
         ([4, 6], 6)]
-
 for arr, max_val in data:
-    print 'inp {}\ttarget max value {}\tmax subarray {}'.format(arr, max_val, max_subarray_xor(arr))
+    print('inp {}\ttarget max value {}\tmax subarray {}'.format(arr, max_val, max_subarray_xor(arr)))
