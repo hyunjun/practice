5fdbf439086e7fa579c0717d04339b36f5d97649
hyunjun <agapelover4u@yahoo.co.kr>
2018-06-24 01:26:53 +0900

    busiest.py; python2 to python3

--- python/problem-busiest-time-in-the-mall/busiest.py
+++ python/problem-busiest-time-in-the-mall/busiest.py
@@ -1,6 +1,6 @@
+#  https://www.pramp.com/question/2WBx3Axln1t7JQ2jQq96
 from collections import defaultdict
 
-#  https://www.pramp.com/question/2WBx3Axln1t7JQ2jQq96
 
 # O(NlogN) time complexity
 # O(N) space complexity
@@ -19,7 +19,7 @@ def busiest_time(data_entries):
   # space
   # sorted_data_entry = sorted(d, key=lambda t: t['time']) # sorted data
   sorted_data_entry = sorted(d.items(), key=lambda t: t[0])
-  print sorted_data_entry
+  print(sorted_data_entry)
 
   cur_cnt = sorted_data_entry[0][1]
   start, end, cur_max = 0, 0, cur_cnt
@@ -34,16 +34,18 @@ def busiest_time(data_entries):
     if cur_max < cur_cnt:
       cur_max = cur_cnt
       end = i
-      print (start, end, cur_max)
+      print((start, end, cur_max))
     if cur_cnt <= 0:
       cur_cnt = 0
 
-  print start, end
+  print(start, end)
   return (sorted_data_entry[start][0], sorted_data_entry[end][0])
+
 
 # input; unsorted
 # time can be multiple
 # type; enter & exit
 inp = [{'time': 1440084737, 'count': 4, 'type': "enter"}, {'time': 1340084737, 'count': 2, 'type': "enter"}, {'time': 1340084737, 'count': 2, 'type': "exit"}, {'time': 1440094731, 'count': 100, 'type': "enter"}, {'time': 1440084733, 'count': 5, 'type': "enter"}, {'time': 1440084834, 'count': 9, 'type': "exit"}, {'time': 1440094735, 'count': 1, 'type': "enter"}, {'time': 1440084737, 'count': 5, 'type': "exit"}, {'time': 1440084837, 'count': 9, 'type': "exit"}, {'time': 1440084637, 'count': 7, 'type': "enter"}, {'time': 1440084737, 'count': 2, 'type': "enter"}]
 
-print busiest_time(inp)
+
+print(busiest_time(inp))
