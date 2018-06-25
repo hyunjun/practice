4ecfb56506cba493bd26980a08c394137e350ab4
hyunjun <agapelover4u@yahoo.co.kr>
2018-06-26 00:07:55 +0900

    [Medium] problem for https://leetcode.com/problems/4sum

--- python/problem-4sum/four_sum.py
+++ python/problem-4sum/four_sum.py
@@ -1,4 +1,4 @@
-#   https://leetcode.com/problems/4sum/
+#   https://leetcode.com/problems/4sum
 
 
 #   32.21%
@@ -99,11 +99,107 @@ def four_sum2(nums, target):
     return result
 
 
+def lower_bound(nums, target):
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if nums[m][1] == target:
+            if (0 <= m - 1 and nums[m - 1][1] < target) or 0 == m:
+                return m
+            r -= 1
+        elif nums[m][1] < target:
+            l += 1
+        else:
+            r -= 1
+    return -1
+
+
+def upper_bound(nums, target):
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if nums[m][1] == target:
+            if m == r or (m + 1 <= r and target < nums[m + 1][1]):
+                return m
+            l += 1
+        elif nums[m][1] < target:
+            l += 1
+        else:
+            r -= 1
+    return -1
+
+
+#   Time Limit Exceeded
+def four_sum3(nums, target):
+    if nums is None or 0 == len(nums):
+        return []
+
+    nums.sort()
+
+    len_nums, two_sum_dict = len(nums), {}
+    for i in range(len_nums - 1):
+        for j in range(i + 1, len_nums):
+            two_sum_dict[(i, j)] = nums[i] + nums[j]
+
+    two_sums = sorted(two_sum_dict.items(), key=lambda t: (t[1], t[0][0], t[0][1]))
+    len_two_sums, result = len(two_sums), []
+    #print(two_sums)
+    for i in range(len_nums - 1):
+        for j in range(i + 1, len_nums):
+            cur_target = target - nums[i] - nums[j]
+            lower_idx = lower_bound(two_sums, cur_target)
+            upper_idx = upper_bound(two_sums, cur_target)
+            #print(cur_target, lower_idx, upper_idx)
+            if -1 == lower_idx or -1 == upper_idx:
+                continue
+            for k in range(lower_idx, upper_idx + 1):
+                ii, jj = two_sums[k][0]
+                if 4 == len(set([i, j, ii, jj])):
+                    cand = sorted([nums[i], nums[j], nums[ii], nums[jj]])
+                    if cand not in result:
+                        result.append(cand)
+    return result
+
+
+class Solution:
+
+    #   62.53%
+    def fourSum(self, nums, target):
+        if nums is None or 0 == len(nums):
+            return []
+
+        nums.sort()
+        lenNums, twoSumDict = len(nums), {}
+        for i in range(lenNums - 1):
+            for j in range(i + 1, lenNums):
+                curVal = nums[i] + nums[j]
+                if curVal in twoSumDict:
+                    twoSumDict[curVal].append((i, j))
+                else:
+                    twoSumDict[curVal] = [(i, j)]
+
+        result = []
+        for val, ijList in twoSumDict.items():
+            curTarget = target - val
+            if curTarget not in twoSumDict:
+                continue
+            klList = twoSumDict[curTarget]
+            for i, j in ijList:
+                for k, l in klList:
+                    if 4 != len(set([i, j, k, l])):
+                        continue
+                    cand = sorted([nums[i], nums[j], nums[k], nums[l]])
+                    if cand not in result:
+                        result.append(cand)
+        return result
+
+
 from data import data
+s = Solution()
 for nums, target, expected in data:
     result0 = sorted(four_sum0(nums, target))
-    result1 = sorted(four_sum1(nums, target))
-    result2 = sorted(four_sum2(nums, target))
-    print(nums, target)
+    result1 = sorted(s.fourSum(nums, target))
+    print('\n', nums, target)
     print(expected)
-    print(sorted(expected) == result0 == result1 == result2)
+    print(result1)
+    print(sorted(expected) == result0 == result1)
