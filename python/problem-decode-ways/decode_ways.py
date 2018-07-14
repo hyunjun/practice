c90c7df780816fc9884e8a521742d2c1d56f09b3
hyunjun <agapelover4u@yahoo.co.kr>
2018-07-15 01:16:33 +0900

    number of ways to decode

--- python/problem-decode-ways/decode_ways.py
+++ python/problem-decode-ways/decode_ways.py
@@ -64,15 +64,7 @@ class Solution:
 
 
 s = Solution()
-cases = [('123', 3),
-         ('1226', 5),
-         ('1206', 1),
-         ('301', 0),
-         ('101', 1),
-         ('611', 2),
-         ('27', 1),
-         ('4757562545844617494555774581341211511296816786586787755257741178599337186486723247528324612117156948', 589824)
-         ]
+from data import cases
 for msg, expected in cases:
     real = s.numDecodings(msg)
     print('{}\texpected {}\treal {}\tresult {}'.format(msg, expected, real, expected == real))
--- python/problem-decode-ways/numOfDecodes.py
+++ python/problem-decode-ways/numOfDecodes.py
@@ -1,32 +1,31 @@
 #   https://leetcode.com/problems/decode-ways
 #   http://www.glassdoor.com/Interview/I-was-given-a-question-about-decoding-which-means-decoding-a-message-with-a-mapping-between-number-and-character-Actuall-QTN_559036.htm
+#   https://www.youtube.com/watch?v=qli-JCrSwuk
 
 
-#   Wrong Answer
-def numOfDecodes(str):
-    if str is None or len(str) == 0:
+def numOfDecodes(s):
+    if s is None or len(s) == 0:
         return 0
 
-    if 1 == len(str):
-        if '0' == str[:1]:
-            return 0
-        return 1
-    if 2 == len(str):
-        if '0' == str[:1]:
-            return numOfDecodes(str[1:])
-        if '1' == str[:1]:
-            if '0' == str[1:]:
-                return 1
-            return 2
-        if '2' == str[:1]:
-            if '0' == str[1:] or '7' <= str[1:]:
-                return 1
-            return 2
-        return 1
-    return numOfDecodes(str[1:]) + numOfDecodes(str[2:])
+    res = [0] * len(s)
+    for i, c in enumerate(s):
+        if 0 == i:
+            if '1' <= c <= '9':
+                res[i] = 1
+        elif 1 == i:
+            if '1' <= c <= '9':
+                res[i] += res[i - 1]
+            if '10' <= s[:i + 1] <= '26':
+                res[i] += 1
+        else:
+            if '1' <= c <= '9':
+                res[i] += res[i - 1]
+            if '10' <= s[i - 1:i + 1] <= '26':
+                res[i] += res[i - 2]
+    return res[-1]
 
 
-from decode_ways import cases
+from data import cases
 for msg, expected in cases:
     real = numOfDecodes(msg)
     print('{}\texpected {}\treal {}\tresult {}'.format(msg, expected, real, expected == real))
