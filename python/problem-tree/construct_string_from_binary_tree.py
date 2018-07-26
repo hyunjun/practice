0e427e1e7bd9b91f17374078e16e425331ff80c5
jun <agapelover4u@yahoo.co.kr>
2018-07-26 10:38:05 +0900

    [Easy] iterative solution for https://leetcode.com/problems/construct-string-from-binary-tree

--- python/problem-tree/construct_string_from_binary_tree.py
+++ python/problem-tree/construct_string_from_binary_tree.py
@@ -22,6 +22,32 @@ class Solution:
         res = _preorder(t)
         return res[1:len(res) - 1]
 
+    #   1.01%
+    def tree2strIter(self, t):
+        if t is None:
+            return ''
+        stack, res = [t], []
+        while stack:
+            cur = stack.pop()
+            if '(' == cur:
+                res.append(cur)
+            if ')' == cur:
+                res.append(cur)
+                while stack and ')' == stack[-1]:
+                    res.append(stack.pop())
+            if cur not in ['(', ')']:
+                res.append(cur.val)
+                if cur.right or cur.left:
+                    if cur.right:
+                        stack.append(')')
+                        stack.append(cur.right)
+                        stack.append('(')
+                    stack.append(')')
+                    if cur.left:
+                        stack.append(cur.left)
+                    stack.append('(')
+        return ''.join([str(r) for r in res])
+
 
 s = Solution()
 
@@ -29,10 +55,14 @@ root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
 root.left.left = TreeNode(4)
+print('1(2(4))(3)')
 print('1(2(4))(3)' == s.tree2str(root))
+print('1(2(4))(3)' == s.tree2strIter(root))
 
 root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
 root.left.right = TreeNode(4)
+print('1(2()(4))(3)')
 print('1(2()(4))(3)' == s.tree2str(root))
+print('1(2()(4))(3)' == s.tree2strIter(root))
