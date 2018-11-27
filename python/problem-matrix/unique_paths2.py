dcbfb693758d2c291899383dfc8efde15d874ecb
jun <agapelover4u@yahoo.co.kr>
2018-11-27 13:43:24 +0900

    [Medium] problem for https://leetcode.com/problems/unique-paths-ii/solution

---
+++ python/problem-matrix/unique_paths2.py
@@ -0,0 +1,63 @@
+#   https://leetcode.com/problems/unique-paths-ii
+
+#   https://leetcode.com/problems/unique-paths-ii/solution
+
+
+class Solution:
+    #   Time Limit Exceeded
+    def uniquePathsWithObstacles0(self, obstacleGrid):
+        R, C = len(obstacleGrid) - 1, len(obstacleGrid[0]) - 1
+        def move(row, column):
+            if 1 == obstacleGrid[row][column]:
+                return
+            if row == R and column == C:
+                self.cnt += 1
+            else:
+                if row < R:
+                    move(row + 1, column)
+                if column < C:
+                    move(row, column + 1)
+
+        self.cnt = 0
+        move(0, 0)
+        return self.cnt
+
+    #   44ms, 45.71%
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        R, C = len(obstacleGrid), len(obstacleGrid[0])
+        if 1 == R and 1 == C:
+            if 0 == obstacleGrid[0][0]:
+                return 1
+        if 1 == obstacleGrid[0][0]:
+            return 0
+        counts = [[0] * C for _ in range(R)]
+        for r in range(1, R):
+            if 1 == obstacleGrid[r][0]:
+                break
+            counts[r][0] = 1
+        for c in range(1, C):
+            if 1 == obstacleGrid[0][c]:
+                break
+            counts[0][c] = 1
+        for r in range(1, R):
+            for c in range(1, C):
+                if 1 == obstacleGrid[r][c]:
+                    continue
+                if 0 == obstacleGrid[r - 1][c] and 0 == obstacleGrid[r][c - 1]:
+                    counts[r][c] = counts[r - 1][c] + counts[r][c - 1]
+                elif 0 == obstacleGrid[r - 1][c]:
+                    counts[r][c] = counts[r - 1][c]
+                elif 0 == obstacleGrid[r][c - 1]:
+                    counts[r][c] = counts[r][c - 1]
+        return counts[R - 1][C - 1]
+
+
+s = Solution()
+data = [([[0,0,0], [0,1,0], [0,0,0]], 2),
+        ([[0]], 1),
+        ([[1, 0]], 0),
+        ([[0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],[0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0],[1,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,1],[0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0],[0,0,0,1,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0],[1,0,1,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0],[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0],[0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1,0,0,0,0,0],[0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],[1,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,0,1],[0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0],[0,1,0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0],[0,1,0,0,0,0,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,1,0,1],[1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,1,0,0,1,0,0,0,0,0,0],[0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,1,1,0,1,0,0,0,0,1,1],[0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0,1],[1,1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1],[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0]], 1637984640),
+        ]
+for grid, expected in data:
+    real = s.uniquePathsWithObstacles(grid)
+    print('expected {}, real {}, result {}'.format(expected, real, expected == real))
