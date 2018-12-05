9e64dbe356ad6485d1ecce5245b5497f1be2ea48
jun <agapelover4u@yahoo.co.kr>
2018-12-05 10:08:34 +0900

    [Easy] problem for https://leetcode.com/problems/sqrtx

--- python/problem-math/sqrt.py
+++ python/problem-math/sqrt.py
@@ -2,27 +2,52 @@
 
 
 class Solution:
-  def mySqrt(self, x):
-    if x == 0 or x == 1:
-      return x
-    if x == 2 or x == 3:
-      return 1
+    def mySqrt0(self, x):
+        if x == 0 or x == 1:
+            return x
+        if x == 2 or x == 3:
+            return 1
 
-    for i in range(2, x // 2 + 1):
-      if i * i == x:
+        for i in range(2, x // 2 + 1):
+            if i * i == x:
+                return i
+            if x < i * i:
+                return i - 1
         return i
-      if x < i * i:
-        return i - 1
-    return i
+
+    def mySqrt1(self, x):
+        def _mysqrt_recur(l, r, target):
+            mid = (l + r) / 2
+            if abs(mid * mid - target) < 0.00001:
+                return mid
+            elif mid * mid < target:
+                return _mysqrt_recur(mid, r, target)
+            return _mysqrt_recur(l, mid, target)
+        return _mysqrt_recur(0, x, x)
+
+    #   96ms, 21.33%
+    def mySqrt(self, x):
+        if 0 == x or 1 == x:
+            return x
+        l, r = 0, x
+        while True:
+            mid = (l + r) / 2
+            if abs(mid * mid - x) < 0.00001:
+                break
+            if mid * mid < x:
+                l = mid
+            else:
+                r = mid
+        return int(mid)
+
 
 class Solution2:
-  def mySqrt(self, x):
     # newton's method
-    r = x
-    while r * r > x:
-      r = (r + x / r) / 2
-    return r
-
+    def mySqrt(self, x):
+        r = x
+        while r * r > x:
+            r = (r + x / r) / 2
+        return r
 
 
 s, s2 = Solution(), Solution2()
