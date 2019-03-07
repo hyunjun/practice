b13ae93b43fac624fd4d18c48271e2255677541c
hchung <hyunjun.chung@gmail.com>
2019-03-07 13:12:17 +0700

    [Medium] problem for https://leetcode.com/problems/sort-characters-by-frequency
    O(n)으로 한다면 Medium, 그렇지 않으면 Easy

---
+++ python/problem-hash/sort_characters_by_frequency.py
@@ -0,0 +1,22 @@
+#   https://leetcode.com/problems/sort-characters-by-frequency
+
+
+from collections import Counter
+
+class Solution:
+    #   runtime; 56ms, 62.66%
+    #   memory; 12.6MB, 86.03%
+    def frequencySort(self, s):
+        if s is None or 0 == len(s):
+            return s
+        return ''.join([c * cnt for c, cnt in sorted(Counter(s).items(), key=lambda t: t[1], reverse=True)])
+
+
+s = Solution()
+data = [('tree', 'eert'),
+        ('cccaaa', 'cccaaa'),
+        ('Aabb', 'bbAa'),
+        ]
+for _s, expected in data:
+    real = s.frequencySort(_s)
+    print('{}, expected {}, real {}, result {}'.format(_s, expected, real, expected == real))
