48f0d37ffcab7816d1e4f8854ba8d0b706ef96bc
hchung <hyunjun.chung@gmail.com>
2019-03-18 08:57:35 +0700

    [Medium] problem for https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks
    두 개 스택을 사용해서 구현하는 거 자체는 쉽지만 time complexity를 어떻게 최소화하느냐가 문제
    peek/pop의 경우 두 번째 스택이 비어있는 경우에만 첫 번째 스택을 거꾸로 넣어주는 부분이 핵심(def move())
    put의 경우는 그냥 무조건 첫 번째 스택에 더해주기만 하면 됨
    기존의 code는 비슷하지만, 매번 두 스택을 오가는 코드로 인해 timeout 발생

--- python/problem-stack-and-queue/a_tale_of_two_stacks.py
+++ python/problem-stack-and-queue/a_tale_of_two_stacks.py
@@ -1,6 +1,67 @@
 #   https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks
 
 
+#   timeout
+class MyQueue0:
+    def __init__(self):
+        self.stack1, self.stack2 = [], []
+        self.peekVal = None
+
+    def peek(self):
+        return self.peekVal
+
+    def pop(self):
+        if 0 == len(self.stack1):
+            return None
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        ret = self.stack2.pop()
+        if 0 < len(self.stack2):
+            self.peekVal = self.stack2[-1]
+        else:
+            self.peekVal = None
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return ret
+
+    def put(self, value):
+        if 0 == len(self.stack1):
+            self.peekVal = value
+        self.stack1.append(value)
+
+
+#   timeout
+class MyQueue1:
+    def __init__(self):
+        self.nStack, self.rStack = [], []
+        self.peekVal = None
+
+    def peek(self):
+        return self.peekVal
+
+    def pop(self):
+        if 0 == len(self.nStack) and 0 == len(self.rStack):
+            return None
+        while self.nStack:
+            self.rStack.append(self.nStack.pop())
+        ret = self.rStack.pop()
+        if 0 < len(self.rStack):
+            self.peekVal = self.rStack[-1]
+        else:
+            self.peekVal = None
+        return ret
+
+    def put(self, value):
+        if 0 == len(self.nStack):
+            if 0 == len(self.rStack):
+                self.peekVal = value
+            else:
+                self.peekVal = self.rStack[-1]
+                while self.rStack:
+                    self.nStack.append(self.rStack.pop())
+        self.nStack.append(value)
+
+
 class MyQueue:
     def __init__(self):
         self.stack1, self.stack2 = [], []
