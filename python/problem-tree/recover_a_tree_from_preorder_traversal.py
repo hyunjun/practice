990fd431bb83209b74ab5171964d0bcba73ede94
hyunjun <agapelover4u@yahoo.co.kr>
2019-04-15 23:18:04 +0700

    [Hard] problem for https://leetcode.com/problems/recover-a-tree-from-preorder-traversal
    손코딩으로는 처음 두 번은 잘 안 되서 다시 생각해보고 재귀를 이용해 작성 후 한 번에 통과
    다만 time complexity가 안 좋아 좀 더 생각해봐야겠음

---
+++ python/problem-tree/recover_a_tree_from_preorder_traversal.py
@@ -0,0 +1,45 @@
+#   https://leetcode.com/problems/recover-a-tree-from-preorder-traversal
+
+
+from TreeNode import TreeNode
+
+
+class Solution:
+    #   runtime; 336ms, 5.34%
+    #   memory; 13.5MB, 100.00%
+    def recoverFromPreorder(self, S):
+        if S is None or 0 == len(S):
+            return None
+
+        def construct(s, numDashes):
+            i, indices = 1, []
+            while i < len(s):
+                if s[i - 1] != '-' and i + numDashes < len(s) and s[i + numDashes] != '-' and all([c == '-' for c in s[i:i + numDashes]]):
+                    indices.append(i)
+                    i += numDashes
+                else:
+                    i += 1
+                if 2 == len(indices):
+                    break
+            if 0 == len(indices):
+                node = TreeNode(s)
+            else:
+                node = TreeNode(s[:indices[0]])
+                if 1 == len(indices):
+                    node.left = construct(s[indices[0] + numDashes:], numDashes + 1)
+                else:   #   2 == len(indices)
+                    node.left = construct(s[indices[0] + numDashes:indices[1]], numDashes + 1)
+                    node.right = construct(s[indices[1] + numDashes:], numDashes + 1)
+            return node
+
+        return construct(S, 1)
+
+
+s = Solution()
+data = [('1-2--3--4-5--6--7', '((( 3 ) 2 ( 4 )) 1 (( 6 ) 5 ( 7 )))'),
+        ('1-2--3---4-5--6---7', '(((( 4 ) 3 x) 2 x) 1 ((( 7 ) 6 x) 5 x))'),
+        ('1-401--349---90--88', '(((( 90 ) 349 x) 401 ( 88 )) 1 x)'),
+        ]
+for S, expected in data:
+    real = s.recoverFromPreorder(S)
+    print('{}, expected {}, real {}, result {}'.format(S, expected, real, expected == str(real)))
