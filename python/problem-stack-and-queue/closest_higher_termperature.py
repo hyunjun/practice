8847be8f18b3ef6c2f5700195e4c6df6d930757f
hchung <hyunjun.chung@gmail.com>
2019-05-23 15:12:22 +0700

    problem for https://www.youtube.com/watch?v=cglGq7rTuPE
    간단하지만 stack에 대해 잘 생각해볼 수 있는 문제

---
+++ python/problem-stack-and-queue/closest_higher_termperature.py
@@ -0,0 +1,25 @@
+#   https://www.youtube.com/watch?v=cglGq7rTuPE
+
+
+def closest_higher_temperatures(temps):
+    if temps is None or 0 == len(temps):
+        return []
+    stack = [(0, temps[0])]
+    i, res = 1, [0] * len(temps)
+    while stack and i < len(temps):
+        while stack and i < len(temps) and stack[-1][0] < i and stack[-1][1] < temps[i]:
+            res[stack[-1][0]] = i - stack[-1][0]
+            stack.pop()
+        stack.append((i, temps[i]))
+        i += 1
+    while stack:
+        res[stack[-1][0]] = 0
+        stack.pop()
+    return res
+
+
+data = [([73, 74, 75, 71, 69, 72, 76, 73], [1, 1, 4, 2, 1, 1, 0, 0]),
+        ]
+for temperatures, expected in data:
+    real = closest_higher_temperatures(temperatures)
+    print('{}, expected {}, real {}, result {}'.format(temperatures, expected, real, expected == real))
