7b7a63318ee7baab61efa7d38cae233d5ef4e5a0
hyunjun.chung <agapelover4u@yahoo.co.kr>
2019-08-08 22:39:04 +0700

    [Hard] problem for https://leetcode.com/problems/longest-chunked-palindrome-decomposition
    debugging을 몇 번 해서 어렵지 않게 해결하긴 했는데 약간 깔끔하지 않다는 생각이 듦
    
    조금 개선해서 속도 향상한 경우 추가
    
    참고할 만한 방식 https://leetcode.com/problems/longest-chunked-palindrome-decomposition/discuss/353581/Intuitive-Python-(Less-than-10-lines)-Greedy

--- python/problem-string/longest_chunked_palindrome_decomposition.py
+++ python/problem-string/longest_chunked_palindrome_decomposition.py
@@ -4,7 +4,7 @@
 class Solution:
     #   runtime; 40ms, 59.31%
     #   memory; 14MB, 100.00%
-    def longestDecomposition(self, text: str) -> int:
+    def longestDecomposition0(self, text: str) -> int:
         if text is None or 0 == len(text):
             return 0
         l, r, splitNum = 0, len(text) - 1, 0
@@ -20,14 +20,32 @@ class Solution:
                     break
         return splitNum
 
+    #   runtime; 83.64%
+    #   memory; 13.8MB, 100.00%
+    def longestDecomposition(self, text: str) -> int:
+        if text is None or 0 == len(text):
+            return 0
+        revText, i, j, splitNum = text[::-1], 0, 0, 0
+        while i < len(text):
+            j = i
+            for k in range(i, len(text)):
+                if text[k] != revText[j]:
+                    continue
+                if text[i:k + 1] == revText[j:j + k + 1 - i][::-1]:
+                    i = k + 1
+                    splitNum += 1
+                    break
+        return splitNum
+
 
 s = Solution()
-data = [('ghiabcdefhelloadamhelloabcdefghi', 7),
-        ('merchant', 1),
-        ('antaprezatepzapreanta', 11),
+data = [('merchant', 1),
         ('aaa', 3),
-        ('abcba', 5),
         ('abba', 4),
+        ('abcba', 5),
+        ('antaprezatepzapreanta', 11),
+        ('ghiabcdefhelloadamhelloabcdefghi', 7),
+        ('vwsuvmbwknmnvwsuvmbwk', 5),
         ]
 for text, expected in data:
     real = s.longestDecomposition(text)
