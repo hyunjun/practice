c4e77f9690ee0374138588b1c5db494f511808e6
hyunjun.chung <agapelover4u@yahoo.co.kr>
2019-08-25 22:39:07 +0700

    [Medium] problem for https://leetcode.com/problems/as-far-from-land-as-possible
    처음에 생각했던 대로 조건별로 거리를 설정하는 걸 0, 0부터 한 번, r - 1, c - 1부터 반대로 한 번 진행해서 완성
    속도 향상을 위해서는 다른 방식을 생각해봐야 할 듯

---
+++ python/problem-matrix/as_far_from_land_as_possible.py
@@ -0,0 +1,61 @@
+#   https://leetcode.com/problems/as-far-from-land-as-possible
+
+
+class Solution(object):
+    #   runtime; 624ms, 52.72%
+    #   memory; 11.9MB, 100.00%
+    def maxDistance(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        R, C = len(grid), len(grid[0])
+        
+        def isValid(r, c):
+            if 0 <= r < R and 0 <= c < C:
+                return True
+            return False
+        
+        def hasNeighborOne(r, c):
+            for nr, nc in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]:
+                if isValid(nr, nc) and grid[nr][nc] == 1:
+                    return True
+            return False
+        
+        dist = [[0] * C for _ in range(R)]
+        def getMinDist(r, c):
+            minDist = float('inf')
+            for nr, nc in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]:
+                if isValid(nr, nc) and dist[nr][nc] > 0:
+                    minDist = min(minDist, dist[nr][nc])
+            return minDist
+
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c]:
+                    continue
+                if hasNeighborOne(r, c):
+                    dist[r][c] = 1
+                    continue
+                minDist = getMinDist(r, c)
+                if minDist != float('inf'):
+                    dist[r][c] = minDist + 1
+
+        for r in range(R - 1, -1, -1):
+            for c in range(C - 1, -1, -1):
+                if grid[r][c]:
+                    continue
+                if hasNeighborOne(r, c):
+                    dist[r][c] = 1
+                    continue
+                minDist = getMinDist(r, c)
+                if minDist != float('inf'):
+                    dist[r][c] = minDist + 1
+
+        maxDist = 0
+        for r in range(R):
+            for c in range(C):
+                maxDist = max(maxDist, dist[r][c])
+        if maxDist == 0:
+            return -1
+        return maxDist
