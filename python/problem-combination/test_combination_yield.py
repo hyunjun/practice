021826bafd09eea76bf7c60a3cb61c3b7a662e7a
hyunjun.chung <hyunjun.chung@agoda.com>
2019-12-24 16:13:30 +0700

    Test of combination and permutation iterator using yield

--- python/problem-combination/test_combination_yield.py
+++ python/problem-combination/test_combination_yield.py
@@ -1,6 +1,31 @@
 #   https://leetcode.com/problems/iterator-for-combination/discuss/452186/Python-generator-yield-and-Python-built-in-cheat
 
 
+class Combi:
+    def __init__(self, s, n):
+        self.gen = self.combi(s, n)
+        self.buffer = next(self.gen, None)
+
+    def combi(self, s, n):
+        if 0 == n:
+            yield ''
+        elif len(s) == n:
+            yield s
+        elif n < len(s):
+            for r in self.combi(s[1:], n - 1):
+                yield s[0] + r
+            for r in self.combi(s[1:], n):
+                yield r
+
+    def next(self):
+        r = ''.join(self.buffer)
+        self.buffer = next(self.gen, None)
+        return r
+
+    def hasNext(self):
+        return self.buffer is not None
+
+
 def combi(s, n):
     if 0 == n:
         yield ''
@@ -13,8 +38,12 @@ def combi(s, n):
             yield r
 
 
-f = combi('abc', 2)
-print(next(f, None))
-print(next(f, None))
-print(next(f, None))
-print(next(f, None))
+c = combi('abc', 2)
+print(next(c, None))
+print(next(c, None))
+print(next(c, None))
+print(next(c, None))
+
+c = Combi('123', 2)
+while c.hasNext():
+    print(c.next())
--- python/problem-permutation/test_permutation_yield.py
+++ python/problem-permutation/test_permutation_yield.py
@@ -1,5 +1,27 @@
 
 
+class Perm:
+    def __init__(self, s):
+        self.gen = self.perm(s)
+        self.buffer = next(self.gen, None)
+
+    def perm(self, s):
+        if 1 == len(s):
+            yield s
+        else:
+            for i, c in enumerate(s):
+                for r in self.perm(s[:i] + s[i + 1:]):
+                    yield c + r
+
+    def next(self):
+        r = ''.join(self.buffer)
+        self.buffer = next(self.gen, None)
+        return r
+
+    def hasNext(self):
+        return self.buffer is not None
+
+
 def perm(s):
     if 1 == len(s):
         yield s
@@ -17,3 +39,7 @@ print(next(p, None))
 print(next(p, None))
 print(next(p, None))
 print(next(p, None))
+
+p = Perm('123')
+while p.hasNext():
+    print(p.next())
