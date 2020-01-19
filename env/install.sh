9fa6de43ef66d4f213ad6632edc696f81f1c2617
Jun <hyunjun.chung@gmail.com>
2020-01-19 19:54:55 +0800

    Add more programs for initial configuration of macbook

--- env/install.sh
+++ env/install.sh
@@ -11,8 +11,21 @@ sudo xattr -dr com.apple.quarantine /Applications/Firefox.app
 sudo xattr -dr com.apple.quarantine /Applications/AppCleaner.app
 sudo xattr -dr com.apple.quarantine /Applications/Spectacle.app
 sudo xattr -dr com.apple.quarantine /Applications/Dropbox.app
-sudo xattr -dr com.apple.quarantine /Applications/Calendars.app
+sudo xattr -dr com.apple.quarantine /Applications/CalendarFree.app
 sudo xattr -dr com.apple.quarantine /Applications/LINE.app
 sudo xattr -dr com.apple.quarantine /Applications/KakaoTalk.app
 sudo xattr -dr com.apple.quarantine /Applications/JetBrains\ Toolbox.app
-sudo xattr -dr com.apple.quarantine /Applications/Docker.app
+sudo xattr -dr com.apple.quarantine /Applications/Docker/Docker\ Quickstart\ Terminal.app
+sudo xattr -dr com.apple.quarantine /Applications/Docker/Kitematic.app
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+npm install --global pure-prompt
+
+curl -sLf https://spacevim.org/install.sh | bash
