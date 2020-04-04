f216e354ef5a6e2cee29c98406fc1b109315f7e0
Jun <hyunjun.chung@gmail.com>
2020-04-04 13:47:34 +0800

    https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3283/

--- python/problem-O-of-n/single_number.py
+++ python/problem-O-of-n/single_number.py
@@ -1,5 +1,4 @@
 #   https://leetcode.com/problems/single-number
-#   95.12%
 
 #   https://leetcode.com/problems/single-number/solution/
 
@@ -8,11 +7,30 @@ from collections import Counter
 
 
 class Solution:
-
-    def singleNumber(self, nums):
+    #   95.12%
+    def singleNumber0(self, nums):
         for num, count in Counter(nums).items():
             if 1 == count:
                 return num
+        return None
+
+    #   https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3283/
+    #   without extra space
+    #   runtime; 96ms, 23.85%
+    #   memory; 16.4MB
+    def singleNumber(self, nums: List[int]) -> int:
+        if nums is None or 0 == len(nums):
+            return None
+        if 1 == len(nums):
+            return nums[0]
+        sortedNums = sorted(nums)
+        for i, n in enumerate(sortedNums):
+            if 0 == i and n != sortedNums[i + 1]:
+                return n
+            elif len(sortedNums) - 1 == i and sortedNums[i - 1] != n:
+                return n
+            elif sortedNums[i - 1] != n and n != sortedNums[i + 1]:
+                return n
         return None
 
 
