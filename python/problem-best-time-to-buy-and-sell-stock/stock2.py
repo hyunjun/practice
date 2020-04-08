19aac187ec1db1045b3a6aef8c8946549ae0d932
Jun <hyunjun.chung@gmail.com>
2020-04-09 00:01:37 +0800

    best time to buy and sell stock II https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3287

--- python/problem-best-time-to-buy-and-sell-stock/stock2.py
+++ python/problem-best-time-to-buy-and-sell-stock/stock2.py
@@ -1,5 +1,4 @@
 #   https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
-#   100.00%
 
 #   https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solution/
 
@@ -8,7 +7,8 @@ import sys
 
 
 class Solution:
-    def maxProfit(self, prices):
+    #   100.00%
+    def maxProfit0(self, prices):
         if prices is None or 0 == len(prices):
             return 0
         b, s = sys.maxsize, sys.maxsize
@@ -27,6 +27,25 @@ class Solution:
         if 0 < curProfit:
             profits.append(curProfit)
         return sum(profits)
+
+    #   https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3287
+    #   runtime; 68ms, 40.43%
+    #   memory; 15.2MB
+    def maxProfit(self, prices):
+        if prices is None or not (1 <= len(prices) <= 3 * 10 ** 4):
+            return 0
+        curMin, curMax, profit = prices[0], None, 0
+        for i in range(1, len(prices)):
+            if prices[i - 1] >= prices[i]:
+                if curMax is not None:
+                    profit += curMax - curMin
+                    curMax = None
+                curMin = prices[i]
+            elif prices[i - 1] < prices[i]:
+                curMax = prices[i]
+        if curMax is not None:
+            profit += curMax - curMin
+        return profit
 
 
 s = Solution()
