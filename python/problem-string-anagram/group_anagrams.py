7523b8c06871320812f00ea5044ae7eaf52b9e43
Jun <hyunjun.chung@gmail.com>
2020-04-09 00:11:56 +0800

    group anagrams https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3288

--- python/problem-string-anagram/group_anagrams.py
+++ python/problem-string-anagram/group_anagrams.py
@@ -1,29 +1,42 @@
 # https://leetcode.com/problems/group-anagrams
-# 36.43%
+
 
 class Solution(object):
-  def groupAnagrams(self, strs):
-    """
-    :type strs: List[str]
-    :rtype: List[List[str]]
-    """
-    if strs is None or 0 == len(strs):
-      return []
-    result_dict, index_dict = {}, {}
-    for i, s in enumerate(strs):
-      key = ''.join(sorted(s))
-      if key in result_dict:
-        result_dict[key].append(s)
-      else:
-        result_dict[key] = [s]
-        index_dict[key] = i
-    result = []
-    for key, i in sorted(index_dict.items(), key=lambda t: t[1]):
-      result.append(sorted(result_dict[key]))
-    return result
+    # 36.43%
+    def groupAnagrams0(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        if strs is None or 0 == len(strs):
+            return []
+        result_dict, index_dict = {}, {}
+        for i, s in enumerate(strs):
+            key = ''.join(sorted(s))
+            if key in result_dict:
+                result_dict[key].append(s)
+            else:
+                result_dict[key] = [s]
+                index_dict[key] = i
+        result = []
+        for key, i in sorted(index_dict.items(), key=lambda t: t[1]):
+            result.append(sorted(result_dict[key]))
+        return result
+
+    #   https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3288
+    #   runtime; 104ms, 72.60%
+    #   memory; 16.8MB
+    def groupAnagrams(self, strs):
+        if strs is None or 0 == len(strs):
+            return [[]]
+        d = defaultdict(list)
+        for s in strs:
+            d[''.join(sorted(s))].append(s)
+        return list(d.values())
+
 
 cases = [(["eat", "tea", "tan", "ate", "nat", "bat"], [["ate", "eat","tea"], ["nat","tan"], ["bat"]])]
 s = Solution()
 for strs, expected in cases:
-  real = s.groupAnagrams(strs)
-  print('{}\texpected {}\treal {}\tresult {}'.format(strs, expected, real, expected == real))
+    real = s.groupAnagrams(strs)
+    print(f'{strs} expected {expected} real {real} result {[sorted(e) for e in expected] == [sorted(r) for r in real]}')
