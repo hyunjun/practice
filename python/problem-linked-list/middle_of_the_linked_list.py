2cd88eb82852e4e793d83f1ec8edf44edeedac06
Jun <hyunjun.chung@gmail.com>
2020-04-08 23:26:03 +0800

    [Easy] https://leetcode.com/problems/middle-of-the-linked-list
    https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3290

--- python/problem-linked-list/middle_of_the_linked_list.py
+++ python/problem-linked-list/middle_of_the_linked_list.py
@@ -8,7 +8,7 @@ from ListNode import ListNode
 
 class Solution:
     #   20.89%
-    def middleNode(self, head):
+    def middleNode0(self, head):
         if head is None:
             return None
         l, cur = [], head
@@ -16,6 +16,36 @@ class Solution:
             l.append(cur)
             cur = cur.next
         return l[len(l) // 2]
+
+    #   https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3290
+    #   runtime; 32ms, 18.70%
+    #   memory; 14MB
+    def middleNode1(self, head):
+        if head is None:
+            return None
+        node, cnt = head, 0
+        while node:
+            cnt += 1
+            node = node.next
+        target = cnt // 2
+        node, cnt = head, 0
+        while cnt < target:
+            cnt += 1
+            node = node.next
+        return node
+
+    #   runtime; 40ms
+    #   memory; 13.7MB
+    def middleNode(self, head):
+        if head is None:
+            return None
+        node1, node2 = head, head
+        while node2.next and node2.next.next:
+            node1 = node1.next
+            node2 = node2.next.next
+        if node2.next is None:
+            return node1
+        return node1.next
 
 
 s = Solution()
