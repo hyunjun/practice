72296de0c05a997dca46b17411ccd179f06744f0
Jun <hyunjun.chung@gmail.com>
2020-04-19 21:41:26 +0800

    [Medium] product of array except self https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3300

--- python/problem-product-of-list-except-the-index/product_of_array_except_self.py
+++ python/problem-product-of-list-except-the-index/product_of_array_except_self.py
@@ -1,10 +1,13 @@
 #   https://leetcode.com/problems/product-of-array-except-self
 
 
+from functools import reduce
+
+
 class Solution:
     #   runtime; 104ms, 40.75%
     #   memory; 20.5MB, 9.07%
-    def productExceptSelf(self, nums):
+    def productExceptSelf0(self, nums):
         if nums is None or 0 == len(nums):
             return []
         asc = [1] * len(nums)
@@ -15,10 +18,26 @@ class Solution:
             dsc[i] = nums[i + 1] * dsc[i + 1]
         return [asc[i] * dsc[i] for i in range(len(nums))]
 
+    #   https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3300
+    #   runtime; 120ms, 82.74%
+    #   memory; 20.3MB
+    def productExceptSelf(self, nums):
+        if nums is None or 0 == len(nums):
+            return []
+        total, res = reduce(lambda a, b: a * b, nums), [0] * len(nums)
+        for i, n in enumerate(nums):
+            if 0 == n:
+                res[i] = reduce(lambda a, b: a * b, nums[:i] + nums[i + 1:])
+            else:
+                res[i] = total // n
+        return res
+
 
 s = Solution()
 data = [([1, 2, 3, 4], [24, 12, 8, 6]),
+        ([0, 0], [0, 0]),
+        ([1, 0], [0, 1]),
         ]
 for nums, expected in data:
     real = s.productExceptSelf(nums)
-    print('{}, expected {}, real {}, result {}'.format(nums, expected, real, expected == real))
+    print(f'{nums}, expected {expected}, real {real}, result {expected == real}')
