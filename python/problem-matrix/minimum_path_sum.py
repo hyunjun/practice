ddd79f6840bd830157954f6337fe6e0b709d786d
Jun <hyunjun.chung@gmail.com>
2020-04-19 22:21:52 +0800

    [Medium] minimum path sum https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3303

--- python/problem-matrix/minimum_path_sum.py
+++ python/problem-matrix/minimum_path_sum.py
@@ -25,7 +25,7 @@ class Solution:
         return sum(listOfLists[row - 1][column - 1])
 
     #   40.41%
-    def minPathSum(self, grid):
+    def minPathSum1(self, grid):
         if grid is None or 0 == len(grid) or grid[0] is None or 0 == len(grid[0]):
             return 0
         row, column = len(grid), len(grid[0])
@@ -47,6 +47,23 @@ class Solution:
                     sumGrid[r][c] += leftSum
         return sumGrid[row - 1][column - 1]
 
+    #   https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3303
+    #   runtime; 88ms, 99.03%
+    #   memory; 15.3MB
+    def minPathSum(self, grid):
+        if grid is None or 0 == len(grid) or 0 == len(grid[0]):
+            return 0
+
+        R, C = len(grid), len(grid[0])
+
+        for r in range(1, R):
+            grid[r][0] += grid[r - 1][0]
+        for c in range(1, C):
+            grid[0][c] += grid[0][c - 1]
+        for r in range(1, R):
+            for c in range(1, C):
+                grid[r][c] += min(grid[r - 1][c], grid[r][c - 1])
+        return grid[-1][-1]
 
 s = Solution()
 inp1 = [[1, 3, 1],
@@ -56,4 +73,4 @@ data = [(inp1, 7),
         ]
 for inp, expected in data:
     real = s.minPathSum(inp)
-    print('expected {}, real {}, result {}'.format(expected, real, expected == real))
+    print(f'expected {expected}, real {real}, result {expected == real}')
