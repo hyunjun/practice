376714ebb6a79a45d214786e35f6d5c3f20e3542
Jun <hyunjun.chung@gmail.com>
2020-04-25 22:33:36 +0800

    [Easy] https://leetcode.com/problems/verifying-an-alien-dictionary

--- python/problem-ETC/verifying_an_alien_dictionary.py
+++ python/problem-ETC/verifying_an_alien_dictionary.py
@@ -3,9 +3,12 @@
 #   https://leetcode.com/problems/verifying-an-alien-dictionary/solution
 
 
+from typing import List
+
+
 class Solution:
     #   72ms, 100.00%
-    def isAlienSorted(self, words, order):
+    def isAlienSorted0(self, words, order):
         d = {}
         for i, c in enumerate(order):
             d[c] = i
@@ -18,6 +21,29 @@ class Solution:
                     break
                 if len(word) <= j or d[preWord[j]] > d[word[j]]:
                     return False
+        return True
+
+    #   runtime; 40ms, 23.14%
+    #   memory; 13.8MB, 5.55%
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        if words is None or not (1 <= len(words) <= 100) or 26 != len(order):
+            return False
+        orderDict = {c: i for i, c in enumerate(order)}
+        for r, word in enumerate(words):
+            if 0 == r:
+                continue
+            for c in range(max(len(words[r - 1]), len(word))):
+                if c == len(words[r - 1]):
+                    break
+                if c == len(word):
+                    return False
+                if words[r - 1][c] == word[c]:
+                    continue
+                a, b = orderDict[words[r - 1][c]], orderDict[word[c]]
+                if a > b:
+                    return False
+                if a < b:
+                    break
         return True
 
 
