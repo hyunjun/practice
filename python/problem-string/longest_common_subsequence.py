5b5d736317c9822b2c39bcf4175b26a0402dda1c
Jun <hyunjun.chung@gmail.com>
2020-04-26 15:30:14 +0800

    longest common subsequence https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3311

--- python/problem-string/longest_common_subsequence.py
+++ python/problem-string/longest_common_subsequence.py
@@ -17,6 +17,22 @@ class Solution:
                     table[r + 1][c + 1] = max(table[r][c + 1], table[r + 1][c])
         return table[-1][-1]
 
+    #   https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3311
+    #   runtime; 412ms, 84.48%
+    #   memory; 21.5MB
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if text1 is None or text2 is None or not (1 <= len(text1) <= 1000) or not (1 <= len(text2) <= 1000):
+            return 0
+        R, C = len(text1) + 1, len(text2) + 1
+        grid = [[0] * C for _ in range(R)]
+        for r in range(1, R):
+            for c in range(1, C):
+                if text1[r - 1] == text2[c - 1]:
+                    grid[r][c] = grid[r - 1][c - 1] + 1
+                else:
+                    grid[r][c] = max(grid[r - 1][c], grid[r][c - 1])
+        return grid[-1][-1]
+
 
 s = Solution()
 data = [('abcde', 'ace', 3),
