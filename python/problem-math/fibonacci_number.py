a698dfd69ccd76b3f59be33a35b3bdd35c5baa2e
Jun <hyunjun.chung@gmail.com>
2020-05-01 00:21:12 +0800

    fibonacci number https://leetcode.com/explore/featured/card/recursion-i/255/recursion-memoization/1661

--- python/problem-math/fibonacci_number.py
+++ python/problem-math/fibonacci_number.py
@@ -4,7 +4,7 @@
 class Solution:
     #   runtime; 52ms, 34.18%
     #   memory; 6.4MB, 88.56%
-    def fib(self, N):
+    def fib0(self, N):
         if 0 == N:
             return 0
         if 1 == N:
@@ -16,6 +16,20 @@ class Solution:
             f_n_1 = f_n
         return f_n
 
+    #   https://leetcode.com/explore/featured/card/recursion-i/255/recursion-memoization/1661
+    #   runtime; 28ms, 75.49%
+    #   memory; 13.7MB
+    def fib(self, N: int) -> int:
+        if N == 0:
+            return 0
+        if N == 1:
+            return 1
+        n0, n1, n = 0, 1, 1
+        for _ in range(2, N + 1):
+            n = n0 + n1
+            n0 = n1
+            n1 = n
+        return n
 
 s = Solution()
 data = [(2, 1),
