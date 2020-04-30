28b0f56fa7ee74228325b1b1d231b1be6a0a52e0
Jun <hyunjun.chung@gmail.com>
2020-04-30 22:50:06 +0800

    swap nodes in pairs https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1681

--- python/problem-linked-list/swap_node_in_pairs.py
+++ python/problem-linked-list/swap_node_in_pairs.py
@@ -28,6 +28,22 @@ class Solution:
             return ret
         return head
 
+    #   https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1681
+    #   runtime; 20ms, 98.23%
+    #   memory; 13.7MB
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+        c, n = head, head.next
+        while c and n:
+            c.val, n.val = n.val, c.val
+            if n.next and n.next.next:
+                c = n.next
+                n = n.next.next
+            else:
+                break
+        return head
+
 
 s = Solution()
 print(s.swapPairs(None))
