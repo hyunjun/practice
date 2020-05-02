8a0ef7003b12b802217cc3de5b8b2169adfe375f
Jun <hyunjun.chung@gmail.com>
2020-05-02 23:21:21 +0800

    [Easy] binary search

--- python/problem-binary-search/binary_search.py
+++ python/problem-binary-search/binary_search.py
@@ -1,14 +1,32 @@
 #   https://leetcode.com/problems/binary-search
 
+#   https://leetcode.com/problems/binary-search/solution
+
 
 class Solution:
     #   75.10%
-    def search(self, nums, target):
+    def search0(self, nums, target):
         if nums is None or 0 == len(nums):
             return -1
         l, r = 0, len(nums) - 1
         while l <= r:
             m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
+
+    #   runtime; 524ms, 5.06%
+    #   memory; 14.8MB, 6.45%
+    def search(self, nums, target):
+        if nums is None or 0 == len(nums):
+            return -1
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = l + (r - l) // 2
             if nums[m] == target:
                 return m
             elif nums[m] < target:
