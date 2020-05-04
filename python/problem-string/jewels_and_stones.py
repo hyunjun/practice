a0f9b9fd60ce38f6add52d6f8081b2996602b3e4
Jun <hyunjun.chung@gmail.com>
2020-05-04 22:02:51 +0800

    jewels and stones https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317

--- python/problem-string/jewels_and_stones.py
+++ python/problem-string/jewels_and_stones.py
@@ -1,21 +1,32 @@
 # https://leetcode.com/problems/jewels-and-stones
-# data 부족으로 %는 안 나옴
+
+
 from collections import Counter
 
 
 class Solution:
-  def numJewelsInStones(self, J, S):
-    if J is None or 0 == len(J) or S is None or 0 == len(S):
-      return 0
+    #   runtime; 76ms
+    def numJewelsInStones(self, J, S):
+        if J is None or 0 == len(J) or S is None or 0 == len(S):
+            return 0
 
-    s, countS = 0, Counter(S)
-    for j in J:
-      s += countS[j]
-    return s
+        s, countS = 0, Counter(S)
+        for j in J:
+            s += countS[j]
+        return s
+
+    #   https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317
+    #   runtime; 20ms, 98.29%
+    #   memory; 14MB
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        if J is None or 0 == len(J) or S is None or 0 == len(S):
+            return 0
+        stones = Counter(S)
+        return sum(stones[j] for j in J)
 
 
 s = Solution()
 data = [("aA", "aAAbbbb", 3), ("z", "ZZ", 0)]
 for J, S, expected in data:
-  real = s.numJewelsInStones(J, S)
-  print(f'{J}, {S}, expected {expected}, real {real}, result {expected == real}')
+    real = s.numJewelsInStones(J, S)
+    print(f'{J}, {S}, expected {expected}, real {real}, result {expected == real}')
