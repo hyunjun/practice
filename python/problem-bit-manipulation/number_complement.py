fa341967c7e891d4d6059fbf8f8c104431027188
Jun <hyunjun.chung@gmail.com>
2020-05-04 22:11:51 +0800

    number complement https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3319

--- python/problem-bit-manipulation/number_complement.py
+++ python/problem-bit-manipulation/number_complement.py
@@ -4,8 +4,8 @@
 
 
 class Solution:
-    #   14.37%
-    def findComplement(self, num):
+    #   runtime; 44ms, 14.37%
+    def findComplement0(self, num):
         res = []
         while num:
             if num & 0x1:
@@ -14,6 +14,12 @@ class Solution:
                 res.append('1')
             num >>= 1
         return int(''.join(res[::-1]), 2)
+
+    #   https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3319
+    #   runtime; 24ms, 89.09%
+    #   memory; 14MB
+    def findComplement(self, num: int) -> int:
+        return int(''.join(['0' if b == '1' else '1' for b in bin(num)[2:]]), 2)
 
 
 s = Solution()
