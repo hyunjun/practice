26b0800f2f2de6d223bd72553fd4be43dfa331eb
Jun <hyunjun.chung@gmail.com>
2020-05-09 21:19:28 +0800

    check if it is a straight line https://leetcode.com/explore/featured/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3323

--- python/problem-math/check_if_it_is_a_straight_line.py
+++ python/problem-math/check_if_it_is_a_straight_line.py
@@ -29,6 +29,26 @@ class Solution(object):
                 return False
         return True
 
+    #   https://leetcode.com/explore/featured/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3323
+    #   runtime; 68ms, 21.58%
+    #   memory; 13.9MB
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        if coordinates is None or not (2 <= len(coordinates) <= 1000):
+            return False
+        slopes = []
+        for i, coordinate in enumerate(coordinates):
+            if 0 == i:
+                continue
+            if coordinate[0] == coordinates[i - 1][0]:
+                slope = float('inf')
+            else:
+                slope = (coordinate[1] - coordinates[i - 1][1]) / (coordinate[0] - coordinates[i - 1][0])
+            if 0 < len(slopes):
+                if slopes[-1] != slope:
+                    return False
+            slopes.append(slope)
+        return True
+
 
 s = Solution()
 data = [([[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]], True),
