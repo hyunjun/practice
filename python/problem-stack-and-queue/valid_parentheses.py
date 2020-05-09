00ec9038601dcc1f58ea6d24f891623f38b3dba9
Jun <hyunjun.chung@gmail.com>
2020-05-09 14:34:03 +0800

    [Easy] valid parentheses

--- python/problem-stack-and-queue/valid_parentheses.py
+++ python/problem-stack-and-queue/valid_parentheses.py
@@ -1,30 +1,50 @@
 #   https://leetcode.com/problems/valid-parentheses
+
+
 #   https://www.hackerrank.com/challenges/ctci-balanced-brackets/problem
 
 
 class Solution(object):
-  def isValid(self, s):
-    if s is None or 0 == len(s):
-      return False
+    #   runtime; 45ms, 8.05%
+    def isValid0(self, s):
+        if s is None or 0 == len(s):
+            return False
 
-    stack = []
-    # if an open parenthesis such as (, {, [, put it into the stack
-    # if a close parenthesis such as ), }, ], pop from the stack, then check matching
-    for c in s:
-      if c in ['(', '{', '[']:
-        stack.append(c)
-        #print('stack {}'.format(stack))
-      elif c in [')', '}', ']']:
+        stack = []
+        # if an open parenthesis such as (, {, [, put it into the stack
+        # if a close parenthesis such as ), }, ], pop from the stack, then check matching
+        for c in s:
+            if c in ['(', '{', '[']:
+                stack.append(c)
+            elif c in [')', '}', ']']:
+                if 0 < len(stack):
+                    p = stack.pop()
+                else:
+                    p = ''
+                if p + c not in ['()', '{}', '[]']:
+                    return False
         if 0 < len(stack):
-          p = stack.pop()
-        else:
-          p = ''
-        #print('p + c {}'.format(p + c))
-        if p + c not in ['()', '{}', '[]']:
-          return False
-    if 0 < len(stack):
-      return False
-    return True
+            return False
+        return True
+
+    #   runtime; 24ms, 91.22%
+    #   memory; 13.9MB, 5.22%
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c in ['(', '{', '[']:
+                stack.append(c)
+            else:
+                isMatched = False
+                if 0 < len(stack):
+                    for l, r in [('(', ')'), ('{', '}'), ('[', ']')]:
+                        if stack[-1] == l and c == r:
+                            stack.pop()
+                            isMatched = True
+                            break
+                if not isMatched:
+                    stack.append(c)
+        return 0 == len(stack)
 
 
 cases = [("[", False),
