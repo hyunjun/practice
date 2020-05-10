6c709b04464193d10e6000ea85cf05bd1147edbc
Jun <hyunjun.chung@gmail.com>
2020-05-10 21:22:31 +0800

    find the town judge https://leetcode.com/explore/featured/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3325

--- python/problem-graph/find_the_town_judge.py
+++ python/problem-graph/find_the_town_judge.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 class Solution:
     #   runtime; 92ms, 76.80%
     #   memory; 14.6MB, 100.00%
-    def findJudge(self, N, trust):
+    def findJudge0(self, N, trust):
         if 0 == N or trust is None or 0 == len(trust):
             return -1
         #   1. graph edge relationships using defaultdict(list)
@@ -32,6 +32,28 @@ class Solution:
                 return i
         return -1
 
+    #   https://leetcode.com/explore/featured/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3325
+    #   runtime; 776ms, 84.16%
+    #   memory; 18.3MB
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        if not (1 <= N <= 1000) or trust is None or not (0 <= len(trust) <= 10000):
+            return -1
+        d = defaultdict(set)
+        for a, b in trust:
+            d[a].add(b)
+        for i in range(1, N + 1):
+            if i in d:
+                continue
+            isJudge = True
+            for j in range(1, N + 1):
+                if i == j:
+                    continue
+                if i not in d[j]:
+                    isJudge = False
+                    break
+            if isJudge:
+                return i
+        return -1
 
 s = Solution()
 data = [(2, [[1, 2]], 2),
