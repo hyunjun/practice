78d9ae1b913fd0a30dfc4baea398d10f08cf5e26
Jun <hyunjun.chung@gmail.com>
2020-06-08 22:26:17 +0800

    power of two https://leetcode.com/explore/featured/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3354

--- python/problem-math/power_of_two.py
+++ python/problem-math/power_of_two.py
@@ -1,11 +1,15 @@
 #   https://leetcode.com/problems/power-of-two
-#   72.31%
-
 
 #   https://leetcode.com/problems/power-of-two/discuss/131043/python-bitwise-solution-beat-97
+#   https://leetcode.com/problems/power-of-two/discuss/676846/Math-or-Bit-or-Recursive-or-1-Line-Code-or-Detailed-Explanations
+
+
+import math
+
 
 class Solution:
-    def isPowerOfTwo(self, n):
+    #   runtime; 44ms, 72.31%
+    def isPowerOfTwo0(self, n):
         if n <= 0:
             return False
         times = 1
@@ -16,9 +20,22 @@ class Solution:
         print('n {} times {}'.format(n, times))
         return n % times == 0
 
+    #   https://leetcode.com/explore/featured/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3354
+    #   runtime; 28ms, 82.51%
+    #   memory; 13.8MB
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        return 2 ** int(math.log(n, 2)) == n
+
 
 s = Solution()
-data = [(0, False), (1, True), (16, True), (-16, False), (218, False)]
+data = [(0, False),
+        (1, True),
+        (16, True),
+        (-16, False),
+        (218, False),
+        ]
 for n, expected in data:
     real = s.isPowerOfTwo(n)
     print('{}, expected {}, real {}, result {}'.format(n, expected, real, expected == real))
