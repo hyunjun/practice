c019bf45536de217f02f4f815495cc7ce719ba37
Jun <hyunjun.chung@gmail.com>
2020-06-09 16:00:23 +0800

    is subsequence https://leetcode.com/explore/featured/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3355

--- python/problem-dynamic-programming/is_subsequence.py
+++ python/problem-dynamic-programming/is_subsequence.py
@@ -18,7 +18,7 @@ class Solution:
 
     #   runtime; 136ms, 61.96%
     #   memory; 17.3MB, 26.67%
-    def isSubsequence(self, s: str, t: str) -> bool:
+    def isSubsequence1(self, s: str, t: str) -> bool:
         if (s is None or 0 == len(s)) and (t is None or 0 == len(t)):
             return True
         if (s is None or 0 == len(s)):
@@ -32,6 +32,24 @@ class Solution:
             if i == lenS:
                 return True
         return i == lenS
+
+    #   https://leetcode.com/explore/featured/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3355
+    #   runtime; 36ms, 68.95%
+    #   memory; 14MB
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if s is None or 0 == len(s):
+            return True
+        if not (0 < len(s) <= 100) or t is None or not (0 <= len(t) <= 10 ** 4):
+            return False
+        t = ''.join([c for c in t if c in set(s)])
+        board = [[0] * (len(s) + 1) for _ in range(len(t) + 1)]
+        for r in range(1, len(board)):
+            for c in range(1, len(board[0])):
+                if s[c - 1] == t[r - 1]:
+                    board[r][c] = 1 + board[r - 1][c - 1]
+                else:
+                    board[r][c] = max(board[r - 1][c], board[r][c - 1])
+        return len(s) == board[-1][-1]
 
 
 solution = Solution()
