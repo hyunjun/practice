fbb236d9e77cf563a393fcd79e634ab13c111f06
Jun <hyunjun.chung@gmail.com>
2020-06-23 22:31:25 +0800

    count complete tree nodes https://leetcode.com/explore/featured/card/june-leetcoding-challenge/542/week-4-june-22nd-june-28th/3369

--- python/problem-tree/count_complete_tree_nodes.py
+++ python/problem-tree/count_complete_tree_nodes.py
@@ -3,10 +3,11 @@
 
 from TreeNode import TreeNode
 
+
 class Solution:
     #   runtime; 140ms, 30.32%
     #   memory; 15.2MB, 66.50%
-    def countNodes(self, root):
+    def countNodes0(self, root):
         if root is None:
             return 0
         cnt, q = 0, [root]
@@ -19,6 +20,22 @@ class Solution:
                 q.append(node.right)
         return cnt
 
+    #   https://leetcode.com/explore/featured/card/june-leetcoding-challenge/542/week-4-june-22nd-june-28th/3369
+    #   runtime; 104ms, 23.81%
+    #   memory; 21.1MB
+    def countNodes(self, root: TreeNode) -> int:
+        
+        self.res = 0
+        def count(node):
+            if node is None:
+                return
+            self.res += 1
+            count(node.left)
+            count(node.right)
+        
+        count(root)
+        return self.res
+
 
 s = Solution()
 root = TreeNode(1)
@@ -27,4 +44,8 @@ root.right = TreeNode(3)
 root.left.left = TreeNode(4)
 root.left.right = TreeNode(5)
 root.right.left = TreeNode(6)
-print(s.countNodes(root))
+data = [(root, 6),
+        ]
+for root, expect in data:
+    real = s.countNodes(root)
+    print(f'{root} expect {expect} real {real} result {expect == real}')
