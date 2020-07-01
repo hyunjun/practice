b4a550e30fca89d9d85407743ddcd82bf64ae4fe
Jun <hyunjun.chung@gmail.com>
2020-07-01 22:34:39 +0800

    arranging coins https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3377

--- python/problem-math/arranging_coins.py
+++ python/problem-math/arranging_coins.py
@@ -1,8 +1,8 @@
 #   https://leetcode.com/problems/arranging-coins
-#   5.44%
 
 
 class Solution:
+    #   5.44%
     def arrangeCoins(self, n):
         if n <= 0:
             return n
@@ -14,9 +14,22 @@ class Solution:
             i += 1
         return None
 
+    #   https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3377
+    #   runtime; 1324ms, 19.62%
+    #   memory; 14.1MB, 5.10%
+    def arrangeCoins(self, n: int) -> int:
+        if n <= 0:
+            return 0
+        m = 1
+        while m * (m + 1) // 2 <= n:
+            m += 1
+        return m - 1
+
 
 s = Solution()
-data = [(5, 2), (8, 3)]
+data = [(5, 2),
+        (8, 3),
+        ]
 for n, expected in data:
     real = s.arrangeCoins(n)
     print('{}, expected {}, real {}, result {}'.format(n, expected, real, expected == real))
