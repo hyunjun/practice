002ec91968c08ed87e574ed964d479e05aa9e9c2
Jun <hyunjun.chung@gmail.com>
2020-07-07 19:04:22 +0800

    island perimeter https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3383

--- python/problem-number-of-islands/island_perimeter.py
+++ python/problem-number-of-islands/island_perimeter.py
@@ -1,9 +1,12 @@
 #   https://leetcode.com/problems/island-perimeter
-#   65.52%
+
+
+from typing import List
 
 
 class Solution:
-    def islandPerimeter(self, grid):
+    #   65.52%
+    def islandPerimeter0(self, grid):
         def numPerimeters(r, c):
             num = 0
             if r == 0 or 0 == grid[r - 1][c]:
@@ -26,6 +29,20 @@ class Solution:
                 res += numPerimeters(r, c)
         return res
 
+    #   https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3383
+    #   runtime; 772ms, 23.60%
+    #   memory; 14MB, 64.55%
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        cnt, R, C = 0, len(grid), len(grid[0])
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == 0:
+                    continue
+                for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                    if not (0 <= nr < R) or not (0 <= nc < C) or grid[nr][nc] == 0:
+                        cnt += 1
+        return cnt
+
 
 s = Solution()
 grid0 = [[0,1,0,0],
@@ -33,6 +50,8 @@ grid0 = [[0,1,0,0],
          [0,1,0,0],
          [1,1,0,0]]
 data = [(grid0, 16),
+        ([[1, 1, 1, 1, 0]], 10),
+        ([[1]], 4),
         ]
 for grid, expected in data:
     real = s.islandPerimeter(grid)
