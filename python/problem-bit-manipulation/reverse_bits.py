cd767e9c5f1c62e82b95c44ade52450fc0ebf0ab
Jun <hyunjun.chung@gmail.com>
2020-07-12 22:52:25 +0800

    reverse bits https://leetcode.com/explore/featured/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3388

--- python/problem-bit-manipulation/reverse_bits.py
+++ python/problem-bit-manipulation/reverse_bits.py
@@ -1,11 +1,11 @@
 #   https://leetcode.com/problems/reverse-bits
-#   33.75%
 
 
 class Solution:
     # @param n, an integer
     # @return an integer
-    def reverseBits(self, n):
+    #   33.75%
+    def reverseBits0(self, n):
         bits, bit = [], 0x1
         for i in range(32):
             print('bit {}, n & bit {}'.format(bit, n & bit))
@@ -16,6 +16,14 @@ class Solution:
             bit <<= 1
         print(bits)
         return int(''.join(bits), 2)
+
+    #   https://leetcode.com/explore/featured/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3388
+    #   runtime; 44ms, 20.96%
+    #   memory; 13.8MB, 47.76%
+    def reverseBits(self, n: int) -> int:
+        numStr = bin(n)[2:]
+        numStr = '0' * (32 - len(numStr)) + numStr
+        return int(numStr[::-1], 2)
 
 
 s = Solution()
