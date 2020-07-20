55c5ff55057ee06fd131afef3e7eed10f1f7e8ee
Jun <hyunjun.chung@gmail.com>
2020-07-20 23:21:52 +0800

    remove linked list elements https://leetcode.com/explore/featured/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3396

--- python/problem-linked-list/remove_linked_list_elements.py
+++ python/problem-linked-list/remove_linked_list_elements.py
@@ -26,7 +26,7 @@ class Solution(object):
     #   https://leetcode.com/explore/featured/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3396
     #   runtime; 72ms, 73.47%
     #   memory; 16.8MB, 68.52%
-    def removeElements(self, head: ListNode, val: int) -> ListNode:
+    def removeElements1(self, head: ListNode, val: int) -> ListNode:
         nodes, n = [], head
         while n:
             if n.val != val:
@@ -41,6 +41,23 @@ class Solution(object):
         nodes[-1].next = None
         return nodes[0]
 
+    #   runtime; 72ms, 73.47%
+    #   memory; 16.8MB, 68.52%
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        vals, n = [], head
+        while n:
+            if n.val != val:
+                vals.append(n.val)
+            n = n.next
+        if 0 == len(vals):
+            return None
+        p, n = None, head
+        for v in vals:
+            n.val = v
+            p, n = n, n.next
+        p.next = None
+        return head
+
 
 s = Solution()
 h1 = ListNode(1)
@@ -50,18 +67,16 @@ h1.next.next.next = ListNode(3)
 h1.next.next.next.next = ListNode(4)
 h1.next.next.next.next.next = ListNode(5)
 h1.next.next.next.next.next.next = ListNode(6)
-print(h1)
-node = s.removeElements(h1, 6)
-print(node)
-node2 = s.removeElements(h1, 1)
-print(node2)
 h2 = ListNode(1)
-node3 = s.removeElements(h2, 1)
-print(node3)
 h3 = ListNode(1)
 h3.next = ListNode(2)
 h3.next.next = ListNode(2)
 h3.next.next.next = ListNode(1)
-print(h3)
-node = s.removeElements(h3, 2)
-print(node)
+data = [(h1, 6),
+        (h2, 1),
+        (h3, 2),
+        ]
+for head, val in data:
+    print(f'{head} {val}')
+    real = s.removeElements(head, val)
+    print(f'\t{real}')
