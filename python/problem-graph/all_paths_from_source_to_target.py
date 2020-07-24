f9934acde39e3f4586ebdea901f895b466bd094d
Jun <hyunjun.chung@gmail.com>
2020-07-24 20:02:55 +0800

    all paths from source to target https://leetcode.com/explore/featured/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3400

--- python/problem-graph/all_paths_from_source_to_target.py
+++ python/problem-graph/all_paths_from_source_to_target.py
@@ -17,9 +17,29 @@ class Solution:
                 q.append((path[:], neighbor))
         return res
 
+    #   https://leetcode.com/explore/featured/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3400
+    #   runtime; 104ms, 87.01%
+    #   memory; 15.4MB, 34.87%
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        res = []
+        
+        def paths(acc, n):
+            if 0 < len(acc) and acc[0] == 0 and n == len(graph) - 1:
+                acc.append(n)
+                res.append(acc[:])
+                acc.pop()
+            else:
+                for m in graph[n]:
+                    acc.append(n)
+                    paths(acc[:], m)
+                    acc.pop()
+        paths([], 0)
+        return res
+
 
 s = Solution()
 data = [([[1, 2], [3], [3], []], [[0, 1, 3], [0, 2, 3]]),
+        ([[4,3,1],[3,2,4],[3],[4],[]], [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]),
         ]
 for graph, expected in data:
     real = s.allPathsSourceToTarget(graph)
