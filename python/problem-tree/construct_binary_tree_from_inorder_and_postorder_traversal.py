243775b175304cc634387e5097b0e5aafa64edaa
Jun <hyunjun.chung@gmail.com>
2020-07-27 17:56:43 +0800

    construct binary tree from inorder and postorder traversal https://leetcode.com/explore/featured/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3403

--- python/problem-tree/construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ python/problem-tree/construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -21,6 +21,17 @@ class Solution:
 
         return buildNode(inorder, postorder)
 
+    #   https://leetcode.com/explore/featured/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3403
+    #   runtime; 204ms, 38.71%
+    #   memory; 87.5MB, 32.32%
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if inorder is None or 0 == len(inorder) or postorder is None or 0 == len(postorder) or len(inorder) != len(postorder):
+            return None
+        node = TreeNode(postorder[-1])
+        idx = inorder.index(node.val)
+        node.left, node.right = self.buildTree(inorder[:idx], postorder[:idx]), self.buildTree(inorder[idx + 1:], postorder[idx:-1])
+        return node
+
 
 s = Solution()
 print(s.buildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]))
