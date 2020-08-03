e84c9085814ee83b182b46e2d23ee35f06404888
Jun <hyunjun.chung@gmail.com>
2020-08-03 21:49:44 +0800

    valid palindrome https://leetcode.com/explore/featured/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3411

--- python/problem-string-palindrome/valid_palindrome.py
+++ python/problem-string-palindrome/valid_palindrome.py
@@ -1,22 +1,36 @@
-# https://leetcode.com/problems/valid-palindrome/
-# 1.58% of python submissions
+# https://leetcode.com/problems/valid-palindrome
+
+
+import re
 
 
 class Solution(object):
-  def isPalindrome(self, s):
-    """
-    :type s: str
-    :rtype: bool
-    """
-    cleaned = []
-    for c in s.lower():
-      if c in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
-        cleaned.append(c)
-    ss = ''.join(cleaned)
-    return ss == ss[::-1]
+    #   1.58% of python submissions
+    def isPalindrome0(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        cleaned = []
+        for c in s.lower():
+            if c in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+                cleaned.append(c)
+        ss = ''.join(cleaned)
+        return ss == ss[::-1]
+
+    #   https://leetcode.com/explore/featured/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3411
+    #   runtime; 36ms, 97.14%
+    #   memory; 15.4MB, 14.20%
+    def isPalindrome(self, s: str) -> bool:
+        r = re.sub('[^a-zA-Z0-9]', '', s).lower()
+        return r == r[::-1]
 
 
 s = Solution()
-for cand, expected in [('', True), ('A man, a plan, a canal: Panama', True), ('race a car', False)]:
-  real = s.isPalindrome(cand)
-  print('{}\texpected {}\treal {}\tresult {}'.format(cand, expected, real, expected == real))
+data = [('', True),
+        ('A man, a plan, a canal: Panama', True),
+        ('race a car', False),
+        ]
+for cand, expect in data:
+    real = s.isPalindrome(cand)
+    print(f'{cand} expect {expect} real {real} result {expect == real}')
