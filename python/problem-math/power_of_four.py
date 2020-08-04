21f78a27d8284b3cdbe59823c04b1909561351f9
Jun <hyunjun.chung@gmail.com>
2020-08-04 20:29:08 +0800

    power of four https://leetcode.com/explore/featured/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3412

--- python/problem-math/power_of_four.py
+++ python/problem-math/power_of_four.py
@@ -1,13 +1,14 @@
 #   https://leetcode.com/problems/power-of-four
-#   49.33%
 
 #   https://leetcode.com/problems/power-of-four/discuss/124313/Three-Python-solutions-(Runtime-56-ms-beats-95.94-of-python3-submissions.)
+
 
 import math
 
 
 class Solution:
-    def isPowerOfFour(self, n):
+    #   49.33%
+    def isPowerOfFour0(self, n):
         if n <= 0:
             return False
         i = math.log(n, 4)
@@ -25,9 +26,33 @@ class Solution:
         return n % times == 0
         '''
 
+    #   RecursionError
+    def isPowerOfFour1(self, num: int) -> bool:
+        if not isinstance(num, int) or num % 4 != 0:
+            return False
+        if num == 4:
+            return True
+        return self.isPowerOfFour(num // 4)
+
+    #   https://leetcode.com/explore/featured/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3412
+    #   runtime; 40ms, 34.75%
+    #   memory; 13.9MB
+    def isPowerOfFour(self, num: int) -> bool:
+        if not isinstance(num, int) or num <= 0 or (1 < num and num % 4 != 0):
+            return False
+        i = math.log(num, 4)
+        return i == (int)(i)
+
 
 s = Solution()
-data = [(0, False), (1, True), (5, False), (8, False), (16, True), (-16, False), (256, True)]
-for n, expected in data:
+data = [(0, False),
+        (1, True),
+        (5, False),
+        (8, False),
+        (16, True),
+        (-16, False),
+        (256, True),
+        ]
+for n, expect in data:
     real = s.isPowerOfFour(n)
-    print('{}, expected {}, real {}, result {}'.format(n, expected, real, expected == real))
+    print(f'{n}, expect {expect}, real {real}, result {expect == real}')
