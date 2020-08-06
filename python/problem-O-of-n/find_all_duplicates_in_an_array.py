2e15ea84ac70f5ef974c0f1b1178975300e02291
Jun <hyunjun.chung@gmail.com>
2020-08-06 19:17:01 +0800

    find all duplicates in an array https://leetcode.com/explore/featured/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3414

--- python/problem-O-of-n/find_all_duplicates_in_an_array.py
+++ python/problem-O-of-n/find_all_duplicates_in_an_array.py
@@ -3,10 +3,13 @@
 #   https://medium.com/solvingalgo/solving-algorithmic-problems-find-a-duplicate-in-an-array-3d9edad5ad41
 
 
+from typing import List
+
+
 class Solution:
     #   runtime; 168ms, 78,59%
     #   memory; 19.9MB, 100.00%
-    def findDuplicates(self, nums):
+    def findDuplicates0(self, nums):
         if nums is None or 0 == len(nums):
             return []
         s, res = set(), []
@@ -17,10 +20,47 @@ class Solution:
                 s.add(n)
         return res
 
+    #   https://leetcode.com/explore/featured/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3414
+    #   runtime; 544ms, 25.20%
+    #   memory; 21MB
+    def findDuplicates1(self, nums: List[int]) -> List[int]:
+        sortedNums, res = [None] * len(nums), []
+        for n in nums:
+            if sortedNums[n - 1] is None:
+                sortedNums[n - 1] = n
+            else:
+                res.append(n)
+        return res
 
+    #   runtime; 860ms
+    #   memory; 20.7MB
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        i, res = 0, set()
+        while i < len(nums):
+            n = nums[i]
+            if n == nums[n - 1]:
+                if i != n - 1:
+                    res.add(n)
+                i += 1
+                continue
+            nums[i], nums[n - 1] = nums[n - 1], nums[i]
+        return list(res)
+            
+            
 s = Solution()
 data = [([4, 3, 2, 7, 8, 2, 3, 1], [2, 3]), 
         ]
-for nums, expected in data:
+for nums, expect in data:
     real = s.findDuplicates(nums)
-    print('{}, expected {}, real {}, result {}'.format(nums, expected, real, expected == real))
+    print(f'{nums}, expect {expect}, real {real}, result {expect == real}')
+'''
+i
+1 2 3 4 5 6 7 8
+4 3 2 7 8 2 3 1
+7     4
+3           7
+2   3
+3 2
+        1     8
+1       3
+'''
