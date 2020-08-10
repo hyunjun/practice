9870e8bd576b19989176b90d0813e107075ad279
Jun <hyunjun.chung@gmail.com>
2020-08-10 23:21:55 +0800

    excel sheet column number https://leetcode.com/explore/featured/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3419

--- python/problem-excel-sheet-columt-title/excel_sheet_column_number.py
+++ python/problem-excel-sheet-columt-title/excel_sheet_column_number.py
@@ -1,12 +1,12 @@
 #   https://leetcode.com/problems/excel-sheet-column-number
-#   12.06%
 
 
 import math
 
 
 class Solution:
-    def titleToNumber(self, s):
+    #   12.06%
+    def titleToNumber0(self, s):
         if s is None or 0 == len(s):
             return 0
         d = {chr(ord('A') + i): i + 1 for i in range(26)}
@@ -16,8 +16,18 @@ class Solution:
             number += math.pow(26, i) * d[c]
         return (int)(number)
 
+    #   https://leetcode.com/explore/featured/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3419
+    #   runtime; 20ms, 99.62%
+    #   memory; 14MB, 6.48%
+    def titleToNumber(self, s: str) -> int:
+        d = {chr(i) : i - ord('A') + 1 for i in range(ord('A'), ord('Z') + 1)}
+        return sum(26 ** i * d[c] for i, c in enumerate(s[::-1]))
+
 s = Solution()
-data = [('A', 1), ('AB', 28), ('ZY', 701)]
-for title, expected in data:
+data = [('A', 1),
+        ('AB', 28),
+        ('ZY', 701),
+        ]
+for title, expect in data:
     real = s.titleToNumber(title)
-    print('{}, expected {}, real {}, result {}'.format(title, expected, real, expected == real))
+    print(f'{title}, expect {expect}, real {real}, result {expect == real}')
