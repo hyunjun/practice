954786b10f49772e1482e04b88e672964f6b207a
Jun <hyunjun.chung@gmail.com>
2020-08-24 16:39:23 +0800

    sum of left leaves https://leetcode.com/explore/featured/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3435

--- python/problem-tree/sum_of_left_leaves.py
+++ python/problem-tree/sum_of_left_leaves.py
@@ -1,5 +1,4 @@
 #   https://leetcode.com/problems/sum-of-left-leaves
-#   25.21%
 
 #   https://leetcode.com/problems/sum-of-left-leaves/discuss/133053/Python-5-line-Simple-recursive-solution
 
@@ -8,7 +7,8 @@ from TreeNode import TreeNode
 
 
 class Solution:
-    def sumOfLeftLeaves(self, root):
+    #   25.21%
+    def sumOfLeftLeaves0(self, root):
         if root is None:
             return 0
         queue, res = [(root, False)], 0
@@ -21,6 +21,23 @@ class Solution:
                 queue.append((cur.left, True))
             if cur.right:
                 queue.append((cur.right, False))
+        return res
+
+    #   https://leetcode.com/explore/featured/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3435
+    #   runtime; 36ms, 60.61%
+    #   memory; 13.9MB, 97.49%
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        res, q = 0, [(root, False)]
+        while q:
+            n, isLeft = q.pop(0)
+            if isLeft and not n.left and not n.right:
+                res += n.val
+            if n.left:
+                q.append((n.left, True))
+            if n.right:
+                q.append((n.right, False))
         return res
 
 
@@ -38,5 +55,8 @@ root.left = TreeNode(9)
 root.right = TreeNode(20)
 root.right.left = TreeNode(15)
 root.right.right = TreeNode(7)
-print(root)
-print(s.sumOfLeftLeaves(root))
+data = [(root, 24),
+        ]
+for root, expect in data:
+    real = s.sumOfLeftLeaves(root)
+    print(f'{root} expect {expect} real {real} result {expect == real}')
