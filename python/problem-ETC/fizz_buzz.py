cf35da2f4b4c3135d0146658b5eea69d69766a82
Jun <hyunjun.chung@gmail.com>
2020-08-26 16:51:17 +0800

    fizz buzz https://leetcode.com/explore/featured/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3437

--- python/problem-ETC/fizz_buzz.py
+++ python/problem-ETC/fizz_buzz.py
@@ -1,11 +1,14 @@
 #   https://leetcode.com/problems/fizz-buzz
-#   27.04%
 
 #   https://leetcode.com/problems/fizz-buzz/discuss/136170/Interesting-Python-1-line-solution-Explained
 
 
+from typing import List
+
+
 class Solution:
-    def fizzBuzz(self, n):
+    #   27.04%
+    def fizzBuzz0(self, n):
         if n <= 0:
             return []
         res = []
@@ -20,6 +23,19 @@ class Solution:
             else:
                 res.append(str(i))
         return res
+
+    #   https://leetcode.com/explore/featured/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3437
+    #   runtime; 52ms, 43.41%
+    #   memory; 14.8MB, 70.24%
+    def fizzBuzz(self, n: int) -> List[str]:
+        def getValue(num):
+            ret = []
+            if num % 3 == 0:
+                ret.append('Fizz')
+            if num % 5 == 0:
+                ret.append('Buzz')
+            return str(num) if 0 == len(ret) else ''.join(ret)
+        return [getValue(i) for i in range(1, n + 1)]
 
 
 s = Solution()
