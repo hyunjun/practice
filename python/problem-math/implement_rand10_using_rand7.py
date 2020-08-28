f2c3f1e1bcc5dec4fbc361cf01c885b17aa20f14
Jun <hyunjun.chung@gmail.com>
2020-08-28 15:40:42 +0800

    implement rand10 using rand7 https://leetcode.com/explore/featured/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3439

--- python/problem-math/implement_rand10_using_rand7.py
+++ python/problem-math/implement_rand10_using_rand7.py
@@ -5,7 +5,7 @@
 
 class Solution:
     #   48.90%
-    def rand10(self):
+    def rand10_0(self):
         """
         :rtype: int
         """
@@ -18,3 +18,16 @@ class Solution:
                 return 10
             return res
         return -1
+
+    #   https://leetcode.com/explore/featured/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3439
+    #   runtime; 332ms, 91.94%
+    #   memory; 16.5MB, 75.41%
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        r = (rand7() - 1) * 7 + rand7()
+        if 1 <= r <= 40:
+            res = r % 10
+            return 10 if res == 0 else res
+        return self.rand10()
