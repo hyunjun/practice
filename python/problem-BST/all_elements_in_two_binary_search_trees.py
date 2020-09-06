6d8b2695fcfa38a0969c370d3131012e82b3f7ab
Hyunjun Chung <hyunjun.chung@gmail.com>
2020-09-06 17:21:01 +0800

    all elements in two binary search trees https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3449

--- python/problem-BST/all_elements_in_two_binary_search_trees.py
+++ python/problem-BST/all_elements_in_two_binary_search_trees.py
@@ -8,7 +8,7 @@ from typing import List
 class Solution:
     #   runtime; 332ms, 93.03%
     #   memory; 16MB, 100.00%
-    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+    def getAllElements0(self, root1: TreeNode, root2: TreeNode) -> List[int]:
 
         def elems(node):
             n, stack, res = node, [], []
@@ -26,6 +26,23 @@ class Solution:
         res.extend(elems(root2))
         return sorted(res)
 
+    #   https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3449
+    #   runtime; 328ms, 99.28%
+    #   memory; 17.1MB, 82.68%
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        def traverse(root):
+            stack, node, res = [], root, []
+            while stack or node:
+                if node:
+                    stack.append(node)
+                    node = node.left
+                else:
+                    node = stack.pop()
+                    res.append(node.val)
+                    node = node.right
+            return res
+        return sorted(traverse(root1) + traverse(root2))
+
 
 s = Solution()
 root1 = TreeNode(2)
@@ -37,6 +54,6 @@ root2.right = TreeNode(3)
 data = [(root1, root2, [0, 1, 1, 2, 3, 4]),
         (None, None, []),
         ]
-for root1, root2, expected in data:
+for root1, root2, expect in data:
     real = s.getAllElements(root1, root2)
-    print(f'{root1} {root2} expected {expected} real {real} result {expected == real}')
+    print(f'{root1} {root2} expect {expect} real {real} result {expect == real}')
