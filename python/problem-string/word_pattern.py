70d496fc12930c4bec7c4bc0d7796c6fc3fe71d3
Hyunjun Chung <hyunjun.chung@gmail.com>
2020-09-07 19:40:00 +0800

    word pattern https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3451

--- python/problem-string/word_pattern.py
+++ python/problem-string/word_pattern.py
@@ -2,8 +2,11 @@
 #   85.49%
 
 
+from collections import defaultdict
+
+
 class Solution:
-    def wordPattern(self, pattern, str):
+    def wordPattern0(self, pattern, str):
         if (None == pattern == str) or (0 == len(pattern) == len(str)):
             return True
         if pattern is None or 0 == len(pattern) or str is None or 0 == len(str):
@@ -22,6 +25,15 @@ class Solution:
                 d[pattern[i]] = s
         return True
 
+    #   https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3451
+    #   runtime; 32ms, 60.84%
+    #   memory; 13.9MB, 26.27%
+    def wordPattern(self, pattern: str, S: str) -> bool:
+        d, strList = defaultdict(set), S.split()
+        for p, s in zip(pattern, strList):
+            d[p].add(s)
+        return len(pattern) == len(strList) and len(set(pattern)) == len(set(strList)) == len(d) and all(len(v) == 1 for v in d.values())
+
 
 s = Solution()
 data = [('abba', 'dog cat cat dog', True),
@@ -29,6 +41,7 @@ data = [('abba', 'dog cat cat dog', True),
         ('aaaa', 'dog cat cat dog', False),
         ('abba', 'dog dog dog dog', False),
         ('aaa', 'aa aa aa aa', False),
+        ('aba', 'dog cat cat', False),
         ]
 for pattern, str, expected in data:
     real = s.wordPattern(pattern, str)
