c8538e24c5987c41c1f698c962a40b0b14db2164
Hyunjun Chung <hyunjun.chung@gmail.com>
2020-09-11 19:21:23 +0800

    maximum product subarray https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/555/week-2-september-8th-september-14th/3456

--- python/problem-highest-product/maximum_product_subarray.py
+++ python/problem-highest-product/maximum_product_subarray.py
@@ -3,12 +3,14 @@
 #   https://leetcode.com/problems/maximum-product-subarray/discuss/153372/Python-concise-O(n)-solution
 
 
+from functools import reduce
+from typing import List
 import sys
 
 
 class Solution:
     #   99.06%
-    def maxProduct(self, nums):
+    def maxProduct0(self, nums):
         if 1 == len(nums):
             return nums[0]
         zeroIndices = [i for i, n in enumerate(nums) if 0 == n]
@@ -54,6 +56,39 @@ class Solution:
                 maxVal = max(maxVal, tmp)
             prevIdx = zIdx + 1
         return maxVal
+
+    #   https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/555/week-2-september-8th-september-14th/3456
+    #   runtime; 44ms, 99.36%
+    #   memory; 14.3MB, 21.34%
+    def maxProduct(self, nums: List[int]) -> int:
+
+        def getProduct(s, e):
+            if s <= e:
+                return reduce(lambda a, b: a * b, nums[s:e + 1])
+            return float('-inf')
+
+        def getMaxProduct(s, e, negs, curMax):
+            if len(negs) % 2 == 0:
+                return max(curMax, getProduct(s, e))
+            if len(negs) == 1:
+                return max(curMax, getProduct(s, negs[0] - 1), getProduct(negs[0] + 1, e))
+            s1, e1 = min(s, negs[0]), negs[-1] - 1 if negs[-1] <= e else e
+            s2, e2 = negs[0] + 1 if s <= negs[0] else s, max(e, negs[-1])
+            return max(curMax, getProduct(s1, e1), getProduct(s2, e2))
+
+        s, e, negs, maxP = 0, 0, [], nums[0]
+        for i, n in enumerate(nums):
+            if n == 0:
+                e, maxP = i - 1, max(maxP, 0)
+                if s <= e:
+                    maxP = getMaxProduct(s, e, negs, maxP)
+                s, negs = i + 1, []
+            else:
+                e = i
+                if n < 0:
+                    negs.append(i)
+        maxP = getMaxProduct(s, len(nums) - 1, negs, maxP)
+        return maxP
 
 
 s = Solution()
