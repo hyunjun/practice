35d1fc4e28769f59f1aae6ac45b9f9453c2469c2
Hyunjun Chung <hyunjun.chung@gmail.com>
2020-09-20 21:34:33 +0800

    unique paths iii https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/556/week-3-september-15th-september-21st/3466

--- python/problem-matrix/unique_paths_III.py
+++ python/problem-matrix/unique_paths_III.py
@@ -3,10 +3,13 @@
 #   https://leetcode.com/problems/unique-paths-iii/solution
 
 
+from typing import List
+
+
 class Solution:
     #   runtime; 60ms, 76.34%
     #   memory; 12.7MB, 100.00%
-    def uniquePathsIII(self, grid):
+    def uniquePathsIII0(self, grid):
         if grid is None or 0 == len(grid) or 0 == len(grid[0]):
             return 0
 
@@ -36,6 +39,34 @@ class Solution:
 
         walk([], set(), sr, sc)
         return len(res)
+
+    #   https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/556/week-3-september-15th-september-21st/3466
+    #   runtime; 92ms, 29.81%
+    #   memory; 19.4MB
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        R, C = len(grid), len(grid[0])
+        pathLen, sr, sc = 0, None, None
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == 1:
+                    sr, sc = r, c
+                if grid[r][c] != -1:
+                    pathLen += 1
+        cnt, queue = 0, [(set(), (sr, sc))]
+        while queue:
+            visited, point = queue.pop(0)
+            if point in visited:
+                continue
+            visited.add(point)
+            if grid[point[0]][point[1]] == 2:
+                if len(visited) == pathLen:
+                    cnt += 1
+                continue
+            for nr, nc in [(point[0] + 1, point[1]), (point[0] - 1, point[1]), (point[0], point[1] + 1), (point[0], point[1] - 1)]:
+                if not (0 <= nr < R) or not (0 <= nc < C) or grid[nr][nc] == -1:
+                    continue
+                queue.append((visited.copy(), (nr, nc)))
+        return cnt
 
 
 s = Solution()
