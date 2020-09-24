8889ad82814cc2074f3d0e7fdbde3a3bf0c44699
Jun <hyunjun.chung@gmail.com>
2020-09-24 16:29:09 +0800

    find the difference https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/557/week-4-september-22nd-september-28th/3471

--- python/problem-string/find_the_difference.py
+++ python/problem-string/find_the_difference.py
@@ -1,13 +1,15 @@
 #   https://leetcode.com/problems/find-the-difference
-#   94.82%
 
 #   https://leetcode.com/problems/find-the-difference/discuss/133924/Python-Bitmap-(XOR)-solution
 #   https://leetcode.com/problems/find-the-difference/discuss/86845/1-liners-and-2-liner-in-Python
 
 
-class Solution:
+from collections import Counter
 
-    def findTheDifference(self, s, t):
+
+class Solution:
+    #   runtime; 24ms, 94.82%
+    def findTheDifference0(self, s, t):
         sDict, tDict = {}, {}
         for c in s:
             if c in sDict:
@@ -24,9 +26,18 @@ class Solution:
                 return c
         return None
 
+    #   https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/557/week-4-september-22nd-september-28th/3471
+    #   runtime; 44ms, 26.53%
+    #   memory; 14MB, 19.65%
+    def findTheDifference(self, s: str, t: str) -> str:
+        return ''.join((Counter(t) - Counter(s)).keys())
+
 
 solution = Solution()
-data = [('', 'y', 'y'), ('a', 'aa', 'a'), ('abcd', 'abcde', 'e')]
-for s, t, expected in data:
+data = [('', 'y', 'y'),
+        ('a', 'aa', 'a'),
+        ('abcd', 'abcde', 'e'),
+        ]
+for s, t, expect in data:
     real = solution.findTheDifference(s, t)
-    print('{}, {}, expected {}, real {}, result {}'.format(s, t, expected, real, expected == real))
+    print(f'{s}, {t}, expect {expect}, real {real}, result {expect == real}')
