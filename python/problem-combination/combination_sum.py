6fa7adc79aa3978a9bbfe50ab57b29740bfd49ac
Jun <hyunjun.chung@gmail.com>
2020-10-02 18:32:54 +0800

    combination sum https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3481

--- python/problem-combination/combination_sum.py
+++ python/problem-combination/combination_sum.py
@@ -7,7 +7,7 @@ from typing import List
 class Solution:
     #   runtime; 96ms, 50.51%
     #   memory; 14MB, 26.21%
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum0(self, candidates: List[int], target: int) -> List[List[int]]:
 
         self.res = []
         def getSum(acc, curSum):
@@ -25,6 +25,27 @@ class Solution:
 
         return self.res
 
+    #   https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3481
+    #   runtime; 112ms, 32.19%
+    #   memory; 14.2MB
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        self.res = []
+        def accSum(acc, curSum):
+            if curSum > target:
+                return
+            if curSum == target:
+                self.res.append(acc[:])
+            else:
+                for c in candidates:
+                    if 0 < len(acc) and c < acc[-1]:
+                        continue
+                    acc.append(c)
+                    accSum(acc, curSum + c)
+                    acc.pop()
+
+        accSum([], 0)
+        return self.res
 
 s = Solution()
 data = [([2, 3, 6, 7], 7, [[7], [2, 2, 3]]),
