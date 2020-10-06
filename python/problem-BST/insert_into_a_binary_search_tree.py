70f301c2e41cf45a2209f828bab0279638506412
Hyunjun Chung <hyunjun.chung@gmail.com>
2020-10-06 19:12:19 +0800

    insert into a binary search tree https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3485

--- python/problem-BST/insert_into_a_binary_search_tree.py
+++ python/problem-BST/insert_into_a_binary_search_tree.py
@@ -3,10 +3,11 @@
 
 from TreeNode import TreeNode
 
+
 class Solution:
     #   runtime; 120ms, 96.12%
     #   memory; 10MB, 67.05%
-    def insertIntoBST(self, root, val):
+    def insertIntoBST0(self, root, val):
         if root is None:
             return None
         node = root
@@ -24,6 +25,28 @@ class Solution:
                 else:
                     node.left = TreeNode(val)
                     break
+        return root
+
+    #   https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3485
+    #   runtime; 128ms, 97.86%
+    #   memory; 16.3MB, 11.15%
+    def insertIntoBST(self, root, val):
+        if root is None:
+            return TreeNode(val)
+
+        def insert(node):
+            if node.val < val:
+                if node.right:
+                    insert(node.right)
+                else:
+                    node.right = TreeNode(val)
+            elif val < node.val:
+                if node.left:
+                    insert(node.left)
+                else:
+                    node.left = TreeNode(val)
+
+        insert(root)
         return root
 
 
