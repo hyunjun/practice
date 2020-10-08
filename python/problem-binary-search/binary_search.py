18f55c16172a48e816fd0a5e80317f7314870eb0
Jun <hyunjun.chung@gmail.com>
2020-10-08 17:39:32 +0800

    binary search https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/560/week-2-october-8th-october-14th/3488

--- python/problem-binary-search/binary_search.py
+++ python/problem-binary-search/binary_search.py
@@ -21,7 +21,7 @@ class Solution:
 
     #   runtime; 524ms, 5.06%
     #   memory; 14.8MB, 6.45%
-    def search(self, nums, target):
+    def search1(self, nums, target):
         if nums is None or 0 == len(nums):
             return -1
         l, r = 0, len(nums) - 1
@@ -30,6 +30,21 @@ class Solution:
             if nums[m] == target:
                 return m
             elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
+
+    #   https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/560/week-2-october-8th-october-14th/3488
+    #   runtime; 228ms, 96.74%
+    #   memory; 15.2MB, 14.76%
+    def search(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
                 l = m + 1
             else:
                 r = m - 1
