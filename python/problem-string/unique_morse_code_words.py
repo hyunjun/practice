10e15ec0063b1a2fb176dc60117308e05adf1702
Jun <hyunjun.chung@gmail.com>
2020-11-22 16:44:06 +0800

    unique morse code words https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/567/week-4-november-22nd-november-28th/3540

--- python/problem-string/unique_morse_code_words.py
+++ python/problem-string/unique_morse_code_words.py
@@ -3,14 +3,26 @@
 #   https://leetcode.com/problems/unique-morse-code-words/solution
 
 
+from typing import List
+
+
 class Solution:
     morseCodes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
 
-    #   11.26%
-    def uniqueMorseRepresentations(self, words):
+    #   runtime; 48ms, 11.26%
+    def uniqueMorseRepresentations0(self, words):
         s = set()
         [s.add(''.join([Solution.morseCodes[ord(c) - ord('a')] for c in word])) for word in words]
         return len(s)
+
+    #   https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/567/week-4-november-22nd-november-28th/3540
+    #   runtime; 36ms, 51.88%
+    #   memory; 14.2MB
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        transformed, morseCodes = set(), [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        for word in words:
+            transformed.add(''.join(morseCodes[ord(c) - ord('a')] for c in word))
+        return len(transformed)
 
 
 s = Solution()
