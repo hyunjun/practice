69ad916d1cc8e0e13c63cf85bf42ee11cfd0499b
Hyunjun Chung <hyunjun.chung@gmail.com>
2020-12-10 23:18:27 +0800

    valid mountain array https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/570/week-2-december-8th-december-14th/3561

--- python/problem-ETC/valid_mountain_array.py
+++ python/problem-ETC/valid_mountain_array.py
@@ -1,6 +1,9 @@
 #   https://leetcode.com/problems/valid-mountain-array
 
 
+from typing import List
+
+
 class Solution:
     #   Wrong Answer
     def validMountainArray0(self, A):
@@ -21,7 +24,7 @@ class Solution:
         return True
 
     #   68ms, 100.00%
-    def validMountainArray(self, A):
+    def validMountainArray1(self, A):
         if len(A) < 3:
             return False
         maxIdx = 0
@@ -36,12 +39,35 @@ class Solution:
                 return False
         return True
 
+    #   https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/570/week-2-december-8th-december-14th/3561
+    #   runtime; 208ms, 28.98%
+    #   memory; 15.6MB
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+        pinnacle = arr[0]
+        for i, a in enumerate(arr):
+            if 0 == i:
+                continue
+            if arr[i - 1] == a:
+                return False
+            if arr[i - 1] > a:
+                pinnacle = i - 1
+                break
+        if pinnacle == 0:
+            return False
+        for i in range(pinnacle + 1, len(arr)):
+            if arr[i - 1] <= arr[i]:
+                return False
+        return True
+
 
 s = Solution()
 data = [([2, 1], False),
         ([3, 5, 5], False),
         ([0, 3, 2, 1], True),
         ([1, 7, 9, 5, 4, 1, 2], False),
+        ([9,8,7,6,5,4,3,2,1,0], False),
         ]
 for A, expected in data:
     real = s.validMountainArray(A)
