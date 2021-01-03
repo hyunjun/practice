4e356227284c531542681a6b189800eab15c233d
Jun <hyunjun.chung@gmail.com>
2021-01-03 11:41:12 +0800

    Find a Corresponding Node of a Binary Tree in a Clone of That Tree https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3590

--- python/problem-tree/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
+++ python/problem-tree/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
@@ -23,7 +23,7 @@ class Solution:
 
     #   runtime; 636ms, 97.89%
     #   memory; 23.5MB, 100.00%
-    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+    def getTargetCopy1(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
         q = [(original, cloned)]
         while q:
             on, cn = q.pop(0)
@@ -33,4 +33,21 @@ class Solution:
                 q.append((on.left, cn.left))
             if on.right:
                 q.append((on.right, cn.right))
+        return None
+
+    #   https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3590
+    #   runtime; 672ms, 12.63%
+    #   memory; 24.2MB, 11.50%
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        q1, q2 = [original], [cloned]
+        while q1 and q2:
+            n1, n2 = q1.pop(), q2.pop()
+            if n1 == target:
+                return n2
+            if n1.left and n2.left:
+                q1.append(n1.left)
+                q2.append(n2.left)
+            if n1.right and n2.right:
+                q1.append(n1.right)
+                q2.append(n2.right)
         return None
