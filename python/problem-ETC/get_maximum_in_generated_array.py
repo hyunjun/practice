e834d417fdd9f100efb4445f39088b09df93488a
Hyunjun Chung <hyunjun.chung@gmail.com>
2021-01-15 22:54:31 +0800

    get maximum in generated array https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/581/week-3-january-15th-january-21st/3605

--- python/problem-ETC/get_maximum_in_generated_array.py
+++ python/problem-ETC/get_maximum_in_generated_array.py
@@ -4,7 +4,7 @@
 class Solution:
     #   runtime; 32ms, 58.79%
     #   memory; 14.2MB, 34.05%
-    def getMaximumGenerated(self, n: int) -> int:
+    def getMaximumGenerated0(self, n: int) -> int:
         nums = []
         for i in range(n + 1):
             if 0 == i:
@@ -16,6 +16,24 @@ class Solution:
             else:
                 nums.append(nums[i // 2] + nums[i // 2 + 1])
         return max(nums)
+
+    #   https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/581/week-3-january-15th-january-21st/3605
+    #   runtime; 28ms, 87.00%
+    #   memory; 14.3MB
+    def getMaximumGenerated(self, n: int) -> int:
+        if n < 0:
+            return []
+        if n == 0 or n == 1:
+            return n
+        nums = [0] * (n + 1)
+        maxNum = nums[1] = 1
+        for i in range(2, n + 1):
+            if i % 2 == 0:
+                nums[i] = nums[i // 2]
+            else:
+                nums[i] = nums[i // 2] + nums[i // 2 + 1]
+            maxNum = max(maxNum, nums[i])
+        return maxNum
 
 
 s = Solution()
