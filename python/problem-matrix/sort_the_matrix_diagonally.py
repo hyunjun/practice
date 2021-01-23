ecf9b5b15f8d9ba14c5ac1c380800449dbf92131
Hyunjun Chung <hyunjun.chung@gmail.com>
2021-01-23 21:27:49 +0800

    sort the matrix diagonally https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/582/week-4-january-22nd-january-28th/3614

--- python/problem-matrix/sort_the_matrix_diagonally.py
+++ python/problem-matrix/sort_the_matrix_diagonally.py
@@ -7,7 +7,7 @@ from typing import List
 class Solution:
     #   runtime; 80ms, 96.75%
     #   memory; 14MB, 100.00%
-    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+    def diagonalSort0(self, mat: List[List[int]]) -> List[List[int]]:
         if mat is None or not (1 <= len(mat) <= 100) or not (1 <= len(mat[0]) <= 100):
             return [[]]
         R, C = len(mat), len(mat[0])
@@ -28,19 +28,43 @@ class Solution:
                 idx += 1
         return mat
 
+    #   https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/582/week-4-january-22nd-january-28th/3614
+    #   runtime; 88ms, 50.25%
+    #   memory; 14.6MB, 49.97%
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        if mat is None or 0 == len(mat) or 0 == len(mat[0]):
+            return mat
+        R, C, startPoints = len(mat), len(mat[0]), [(0, 0)]
+        startPoints.extend((0, c) for c in range(1, C))
+        startPoints.extend((r, 0) for r in range(1, R))
+        for r, c in startPoints:
+            y, x, data = r, c, []
+            while y < R and x < C:
+                data.append(mat[y][x])
+                y += 1
+                x += 1
+            data.sort()
+            y, x, i = r, c, 0
+            while y < R and x < C:
+                mat[y][x] = data[i]
+                y += 1
+                x += 1
+                i += 1
+        return mat
+
 
 s = Solution()
 data = [([[3,3,1,1],[2,2,1,2],[1,1,1,2]], [[1,1,1,1],[1,2,2,2],[1,2,3,3]]),
         ([[11,25,66,1,69,7],[23,55,17,45,15,52],[75,31,36,44,58,8],[22,27,33,25,68,4],[84,28,14,11,5,50]], [[5,17,4,1,52,7],[11,11,25,45,8,69],[14,23,25,44,58,15],[22,27,31,36,50,66],[84,28,75,33,55,68]]),
         ]
-for mat, expected in data:
+for mat, expect in data:
     print()
     for m in mat:
         print(m)
     real = s.diagonalSort(mat)
-    print()
-    for e in expected:
+    print('expect')
+    for e in expect:
         print(e)
-    print()
+    print('real')
     for r in real:
         print(r)
