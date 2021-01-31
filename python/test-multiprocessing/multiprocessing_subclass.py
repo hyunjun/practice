31611c0711fad9a44bca2177718b7b142f67a027
Jun <hyunjun.chung@gmail.com>
2021-01-31 23:12:58 +0800

    Adjust indentation, add more logs

--- python/test-multiprocessing/multiprocessing_subclass.py
+++ python/test-multiprocessing/multiprocessing_subclass.py
@@ -1,19 +1,20 @@
 #   https://pymotw.com/3/multiprocessing/basics.html
 import multiprocessing
+import os
 
 
 class Worker(multiprocessing.Process):
 
-  def run(self):
-    print('In {}'.format(self.name))
-    return
+    def run(self):
+        print(f'{os.getpid()} In {self.name}')
+        return
 
 
 if __name__ == '__main__':
-  jobs = []
-  for i in range(5):
-    p = Worker()
-    jobs.append(p)
-    p.start()
-  for j in jobs:
-    j.join()
+    jobs = []
+    for i in range(5):
+        p = Worker()
+        jobs.append(p)
+        p.start()
+    for j in jobs:
+        j.join()
--- python/test-multiprocessing/test_shared_memory.py
+++ python/test-multiprocessing/test_shared_memory.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process, Semaphore, shared_memory
 import numpy as np
+import os
 import time
 
 
@@ -11,13 +12,14 @@ class Reader(Process):
 
         self.shm = shared_memory.SharedMemory(name=shmName)
         self.b = np.ndarray(a.shape, dtype=a.dtype, buffer=self.shm.buf) 
-        print('Reader init. name:', self.shm.name, self.b.shape, self.b)
+        print('Reader init. name:', os.getpid(), self.name, self.shm.name, self.b.shape, self.b)
+
         time.sleep(2)
 
     def run(self):            
-        print('Reader run 0:', self.shm.name, self.b.shape, self.b)
+        print('Reader run 0:', self.name, self.shm.name, self.b.shape, self.b)
         time.sleep(2)
-        print('Reader run 1:', self.shm.name, self.b.shape, self.b)
+        print('Reader run 1:', self.name, self.shm.name, self.b.shape, self.b)
         time.sleep(1)
         self.shm.close()
 
@@ -28,13 +30,13 @@ class Worker(Process):
 
         self.shm = shared_memory.SharedMemory(name=shmName) 
         self.b = np.ndarray(a.shape, dtype=a.dtype, buffer=self.shm.buf) 
-        print('Worker init. name:', self.shm.name, self.b.shape, self.b)
+        print('Worker init. name:', os.getpid(), self.name, self.shm.name, self.b.shape, self.b)
         time.sleep(2)
 
     def run(self):            
-        print('Worker run 0:', self.shm.name, self.b.shape, self.b)
+        print('Worker run 0:', self.name, self.shm.name, self.b.shape, self.b)
         self.b[-1] = 888    #   This is NOT shared with Reader & main processes. Why?
-        print('Worker run 1:', self.shm.name, self.b.shape, self.b)
+        print('Worker run 1:', self.name, self.shm.name, self.b.shape, self.b)
         time.sleep(1)
         self.shm.close()
 
