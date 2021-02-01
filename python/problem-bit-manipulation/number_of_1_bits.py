d827aa1a7f6d317d801d63ce31c4ddb50cb6d63e
Hyunjun Chung <hyunjun.chung@gmail.com>
2021-02-01 23:01:09 +0800

    number of 1 bits https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3625

--- python/problem-bit-manipulation/number_of_1_bits.py
+++ python/problem-bit-manipulation/number_of_1_bits.py
@@ -1,11 +1,11 @@
 #   https://leetcode.com/problems/number-of-1-bits
-#   74.76%
 
 #   https://leetcode.com/problems/number-of-1-bits/solution
 
 
 class Solution:
-    def hammingWeight(self, n):
+    #   74.76%
+    def hammingWeight0(self, n):
         cnt, bit = 0, 0x1
         while bit <= n:
             if 0 != n & bit:
@@ -13,9 +13,17 @@ class Solution:
             bit <<= 1
         return cnt
 
+    #   https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3625
+    #   runtime; 44ms, 10.16%
+    #   memory; 14.1MB, 70.08%
+    def hammingWeight(self, n: int) -> int:
+        return bin(n).count('1')
+
 
 s = Solution()
-data = [(11, 3), (128, 1)]
-for n, expected in data:
+data = [(11, 3),
+        (128, 1),
+        ]
+for n, expect in data:
     real = s.hammingWeight(n)
-    print('{}, expected {}, real {}, result {}'.format(n, expected, real, expected == real))
+    print(f'{n}, expect {expect}, real {real}, result {expect == real}')
