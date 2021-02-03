2b2a496c4484d0ad2c3482072142b93bc511a965
Jun <hyunjun.chung@gmail.com>
2021-02-03 21:42:18 +0800

    linked list cycle https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3627

--- python/problem-linked-list/cycle.py
+++ python/problem-linked-list/cycle.py
@@ -23,3 +23,43 @@ def contains_cycle(node):
         step1 = step1.next
         step2 = step2.next.next
     return False
+
+
+from ListNode import ListNode
+
+
+class Solution:
+    #   https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3627
+    #   runtime; 76ms, 11.77%
+    #   memory; 17.2MB, 67.44%
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+        s, f = head, head.next
+        while s and f:
+            if s == f:
+                return True
+            s = s.next
+            if f.next is None:
+                return False
+            f = f.next.next
+        return False
+
+
+s = Solution()
+head1 = ListNode(3)
+head1.next = ListNode(2)
+head1.next.next = ListNode(0)
+head1.next.next.next = ListNode(-4)
+head1.next.next.next.next = head1.next
+head2 = ListNode(1)
+head2.next = ListNode(2)
+head2.next.next = head2
+head3 = ListNode(1)
+data = [(head1, True),
+        (head2, True),
+        (head3, False),
+        ]
+for head, expect in data:
+    real = s.hasCycle(head)
+    print(f'{head.val} expect {expect} real {real} result {expect == real}')
