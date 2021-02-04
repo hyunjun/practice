1bacf9efeac8ae52700091d0170d27edf14fb3b9
Hyunjun Chung <hyunjun.chung@gmail.com>
2021-02-04 21:16:32 +0800

    longest harmonious subsequence https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3628

--- python/problem-O-of-n/longest_harmonious_subsequence.py
+++ python/problem-O-of-n/longest_harmonious_subsequence.py
@@ -3,9 +3,13 @@
 #   https://leetcode.com/problems/longest-harmonious-subsequence/solution
 
 
+from collections import defaultdict
+from typing import List
+
+
 class Solution:
     #   55.60%
-    def findLHS(self, nums):
+    def findLHS0(self, nums):
         d = {}
         for i, n in enumerate(nums):
             if n in d:
@@ -20,10 +24,29 @@ class Solution:
                 maxLen = max(maxLen, cnt + d[n + 1])
         return maxLen
 
+    #   https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3628
+    #   runtime; 484ms, 8.46%
+    #   memory; 17.5MB
+    def findLHS(self, nums: List[int]) -> int:
+        if nums is None or 0 == len(nums):
+            return 0
+        d = defaultdict(list)
+        for i, n in enumerate(nums):
+            d[n].append(i)
+        l, maxLen = sorted(d.items()), 0
+        for i, (k, idxList) in enumerate(l):
+            if 0 == i:
+                continue
+            if k - l[i - 1][0] == 1:
+                maxLen = max(maxLen, len(idxList) + len(l[i - 1][1]))
+        return maxLen
+
 
 s = Solution()
 data = [([1, 3, 2, 2, 5, 2, 3, 7], 5),
+        ([1, 2, 3, 4], 2),
+        ([1, 1, 1, 1], 0),
         ]
-for nums, expected in data:
+for nums, expect in data:
     real = s.findLHS(nums)
-    print('{}, expected {}, real {}, result {}'.format(nums, expected, real, expected == real))
+    print(f'{nums} expect {expect}, real {real}, result {expect == real}')
