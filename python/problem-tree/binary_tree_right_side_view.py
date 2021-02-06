ccc0908b991acde0b11c87f39c767cdcc8d79b30
Hyunjun Chung <hyunjun.chung@gmail.com>
2021-02-06 19:18:25 +0800

    binary tree right side view https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3630

--- python/problem-tree/binary_tree_right_side_view.py
+++ python/problem-tree/binary_tree_right_side_view.py
@@ -4,11 +4,13 @@
 
 
 from TreeNode import TreeNode
+from collections import defaultdict
+from typing import List
 
 
 class Solution:
     #   100.00%
-    def rightSideView(self, root):
+    def rightSideView0(self, root):
         if root is None:
             return []
         res, queue = [], [(root, 0)]
@@ -23,6 +25,23 @@ class Solution:
             if cur.right:
                 queue.append((cur.right, depth + 1))
         return res
+
+    #   https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3630
+    #   runtime; 32ms, 71.27%
+    #   memory; 14.3MB, 55.13%
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return []
+        q, d, maxDepth = [(0, root)], defaultdict(int), 0
+        while q:
+            depth, node = q.pop(0)
+            maxDepth = max(maxDepth, depth)
+            d[depth] = node.val
+            if node.left:
+                q.append((depth + 1, node.left))
+            if node.right:
+                q.append((depth + 1, node.right))
+        return [d[i] for i in range(maxDepth + 1)]
 
 
 s = Solution()
