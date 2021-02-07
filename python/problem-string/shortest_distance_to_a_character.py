271ee824e33d161308df15636f398029a1bb92a5
Jun <hyunjun.chung@gmail.com>
2021-02-07 20:11:59 +0800

    shortest distance to a character https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3631

--- python/problem-string/shortest_distance_to_a_character.py
+++ python/problem-string/shortest_distance_to_a_character.py
@@ -3,6 +3,9 @@
 #   https://leetcode.com/problems/shortest-distance-to-a-character/solution
 
 
+from typing import List
+
+
 class Solution:
     #   78.70%
     def shortestToChar(self, S, C):
@@ -23,10 +26,30 @@ class Solution:
                 res[i] = min(res[i], abs(cIdx - i))
         return res
 
+    #   https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3631
+    #   runtime; 40ms, 82.09%
+    #   memory; 14.4MB, 61.15%
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        ret = [float('inf')] * len(s)
+        for i, ch in enumerate(s):
+            if ch == c:
+                l, lDistance, r, rDistance = i - 1, 1, i + 1, 1
+                ret[i] = 0
+                while 0 <= l and s[l] != c:
+                    ret[l] = min(ret[l], lDistance)
+                    l -= 1
+                    lDistance += 1
+                while r <= len(s) - 1 and s[r] != c:
+                    ret[r] = min(ret[r], rDistance)
+                    r += 1
+                    rDistance += 1
+        return ret
+
 
 s = Solution()
 data = [('loveleetcode', 'e', [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]),
+        ('aaab', 'b', [3, 2, 1, 0]),
         ]
-for S, C, expected in data:
+for S, C, expect in data:
     real = s.shortestToChar(S, C)
-    print('{}, {}, expected {}, real {}, result {}'.format(S, C, expected, real, expected == real))
+    print(f'{S}, {C}, expect {expect}, real {real}, result {expect == real}')
