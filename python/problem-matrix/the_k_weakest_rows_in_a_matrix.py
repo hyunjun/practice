15e7840d2e41595f8783962acecaf307ea46712f
Jun <hyunjun.chung@gmail.com>
2021-02-15 19:30:59 +0800

    the k weakest rows in a matrix https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3641

--- python/problem-matrix/the_k_weakest_rows_in_a_matrix.py
+++ python/problem-matrix/the_k_weakest_rows_in_a_matrix.py
@@ -7,7 +7,7 @@ from typing import List
 class Solution:
     #   runtime; 116ms, 46.49%
     #   memory; 12.9MB, 100.00%
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+    def kWeakestRows0(self, mat: List[List[int]], k: int) -> List[int]:
         R, C = len(mat), len(mat[0])
         if not (2 <= R <= 100 or 2 <= C <= 100 or 1 <= k <= R):
             return []
@@ -30,6 +30,12 @@ class Solution:
                 if len(res) == k:
                     break
         return res
+
+    #   https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3641
+    #   runtime; 112ms, 48.13%
+    #   memory; 14.6MB, 76.29%
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        return [i for i, _ in sorted([(i, row) for i, row in enumerate(mat)], key=lambda t: (t[1].count(1), t[0]))[:k]]
 
 
 s = Solution()
@@ -62,8 +68,8 @@ data = [(mat1, 3, [2, 0, 3]),
         (mat4, 4, [0, 1, 2, 3]),
         (mat5, 6, [4, 6, 0, 1, 2, 3]),
         ]
-for mat, k, expected in data:
+for mat, k, expect in data:
     real = s.kWeakestRows(mat, k)
     for m in mat:
         print(m)
-    print(f'\t{k} expected {expected} real {real} result {expected == real}')
+    print(f'\t{k} expect {expect} real {real} result {expect == real}')
