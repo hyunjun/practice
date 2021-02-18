14278be0ed99bcdb9701d4a10404f37ec4747c0d
Jun <hyunjun.chung@gmail.com>
2021-02-18 17:11:01 +0800

    container with most water https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3643

--- python/problem-O-of-n/container_with_most_water.py
+++ python/problem-O-of-n/container_with_most_water.py
@@ -1,13 +1,16 @@
 #   https://leetcode.com/problems/container-with-most-water
-#   71.00%
+
+
+from collections import defaultdict
+from typing import List
 
 
 class Solution:
-
+    #   runtime; 71.00% -> 168ms, 75.03%
+    #   memory; 16.4MB, 90.84%
     def maxArea(self, height):
         if height is None or len(height) < 2:
             return 0
-
         maxSize, l, r = 0, 0, len(height) - 1
         while l < r:
             maxSize = max(maxSize, (r - l) * min(height[l], height[r]))
@@ -17,9 +20,29 @@ class Solution:
                 r -= 1
         return maxSize
 
+    #   https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3643
+    #   Time Limit Exceeded
+    def maxArea1(self, height: List[int]) -> int:
+        if height is None or len(height) < 2:
+            return 0
+        d, maxSize = defaultdict(list), 0
+        for h in range(max(height), min(height) - 1, -1):
+            l, r = 0, len(height) - 1
+            while height[l] < h:
+                l += 1
+            while height[r] < h:
+                r -= 1
+            maxSize = max(maxSize, (r - l) * min(height[l], height[r]))
+        return maxSize
 
-data = [([1, 2, 3, 4, 5], 6), ([5, 2, 3, 4, 5], 20), ([1, 1], 1), ([1, 2, 1], 2), ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49)]
+
+data = [([1, 2, 3, 4, 5], 6),
+        ([5, 2, 3, 4, 5], 20),
+        ([1, 1], 1),
+        ([1, 2, 1], 2),
+        ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+        ]
 s = Solution()
-for height, expected in data:
+for height, expect in data:
     real = s.maxArea(height)
-    print('{}, expected {}, real {}, result {}'.format(height, expected, real, expected == real))
+    print(f'{height}, expect {expect}, real {real}, result {expect == real}')
