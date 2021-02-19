1d0996c4a06b7809f5f7bc1e25fa0fe8a1790b59
Jun <hyunjun.chung@gmail.com>
2021-02-19 17:01:37 +0800

    minimum remove to make valid parentheses https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3645

--- python/problem-stack-and-queue/minimum_remove_to_make_valid_parentheses.py
+++ python/problem-stack-and-queue/minimum_remove_to_make_valid_parentheses.py
@@ -4,7 +4,7 @@
 class Solution(object):
     #   runtime; 148ms, 86.80%
     #   memory; 14.6MB, 100.00%
-    def minRemoveToMakeValid(self, s):
+    def minRemoveToMakeValid0(self, s):
         """
         :type s: str
         :rtype: str
@@ -27,6 +27,23 @@ class Solution(object):
             l[i] = ''
         return ''.join(l)
 
+    #   https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3645
+    #   runtime; 172ms, 26.70%
+    #   memory; 25.1MB
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack, positions = [], set()
+        for i, c in enumerate(s):
+            if c == ')':
+                if 0 < len(stack) and stack[-1][0] == '(':
+                    _, oi = stack.pop()
+                    positions.add(oi)
+                    positions.add(i)
+            elif c == '(':
+                stack.append((c, i))
+            else:
+                positions.add(i)
+        return ''.join(c for i, c in enumerate(s) if i in positions)
+
 
 solution = Solution()
 data = [("lee(t(c)o)de)", "lee(t(c)o)de"),
@@ -34,6 +51,6 @@ data = [("lee(t(c)o)de)", "lee(t(c)o)de"),
         ("))((", ""),
         ("(a(b(c)d)", "a(b(c)d)"),
         ]
-for s, expected in data:
+for s, expect in data:
     real = solution.minRemoveToMakeValid(s)
-    print(f'{s}, expected {expected}, real {real}, result {expected == real}')
+    print(f'{s}, expect {expect}, real {real}, result {expect == real}')
