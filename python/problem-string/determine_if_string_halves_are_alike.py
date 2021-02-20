02a11b272f5e43f027915af3dd2cc67df9a3ad5e
Hyunjun Chung <hyunjun.chung@gmail.com>
2021-02-20 19:38:59 +0800

    [Easy] https://leetcode.com/problems/determine-if-string-halves-are-alike

--- python/problem-string/determine_if_string_halves_are_alike.py
+++ python/problem-string/determine_if_string_halves_are_alike.py
@@ -4,8 +4,21 @@
 class Solution:
     #   runtime: 36 ms, 65.55%
     #   memory: 14.3 MB, 68.01%
-    def halvesAreAlike(self, s: str) -> bool: 
+    def halvesAreAlike0(self, s: str) -> bool: 
         m, s, vowels, c = len(s) // 2, s.lower(), set(['a', 'e', 'i', 'o', 'u']), 0
+        for i in range(m):
+            if s[i] in vowels:
+                c += 1
+        for i in range(m, len(s)):
+            if s[i] in vowels:
+                c -= 1
+        return c == 0
+
+    #   lower()를 사용하지 않고 vowels에서 대소문자 모두 처리
+    #   runtime: 32 ms, 85.83%
+    #   memory Usage: 14.5 MB, 9.61%
+    def halvesAreAlike(self, s: str) -> bool: 
+        m, vowels, c = len(s) // 2, set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']), 0
         for i in range(m):
             if s[i] in vowels:
                 c += 1
