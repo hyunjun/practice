b8d8d652f18e95083eb761fee7987bd09e54d074
Hyunjun Chung <hyunjun.chung@gmail.com>
2021-02-25 23:18:12 +0800

    score of parentheses https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3651

--- python/problem-string/score_of_parentheses.py
+++ python/problem-string/score_of_parentheses.py
@@ -6,7 +6,7 @@
 class Solution:
     #   runtime; 36ms, 81.00%
     #   memory; 12.5MB, 100.00%
-    def scoreOfParentheses(self, S):
+    def scoreOfParentheses0(self, S):
         if S is None or 0 == len(S):
             return 0
         stack = []
@@ -29,6 +29,28 @@ class Solution:
         while stack:
             res += stack.pop()
         return res
+
+    #   https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3651
+    #   runtime: 20ms, 99.39%
+    #   memory: 14.3MB
+    def scoreOfParentheses(self, S: str) -> int:
+        stack = []
+        for c in S:
+            if c == '(':
+                stack.append(c)
+            else:
+                if 0 < len(stack) and stack[-1] == '(':
+                    stack.pop()
+                    stack.append(1)
+                else:
+                    num = 0
+                    while 0 < len(stack) and stack[-1] != '(':
+                        num += stack.pop()
+                    if 0 < len(stack) and stack[-1] == '(':
+                        stack.pop()
+                        num *= 2
+                    stack.append(num)
+        return sum(stack)
 
 
 s = Solution()
