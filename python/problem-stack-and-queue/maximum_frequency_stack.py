5742baf15d03a873866f460b565077f5a200df9a
Hyunjun Chung <hyunjun.chung@gmail.com>
2021-03-01 21:46:43 +0800

    maximum frequency stack https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3655

--- python/problem-stack-and-queue/maximum_frequency_stack.py
+++ python/problem-stack-and-queue/maximum_frequency_stack.py
@@ -39,11 +39,13 @@ class FreqStack0(object):
         self.indices[maxIdxVal].pop()
         return maxIdxVal
 
+
 from collections import defaultdict
+
 
 #   runtime; 11160ms, 5.16%
 #   memory; 19.4MB, 10.00%
-class FreqStack:
+class FreqStack1:
 
     def __init__(self):
         self.maxCnt, self.counter, self.indices = 0, defaultdict(int), defaultdict(list)
@@ -62,6 +64,60 @@ class FreqStack:
         self.maxCnt = max(self.maxCnt - 1, max(self.counter.values()))
         #print(self.counter, self.indices, self.maxCnt)
         return num
+
+
+#   Time Limit Exceeded
+class FreqStack2:
+
+    def __init__(self):
+        self.stack, self.freq = [], defaultdict(int)
+
+    def push(self, x):
+        self.stack.append(x)
+        self.freq[x] += 1
+
+    def pop(self):
+        freqList = self.freq.items()
+        maxCnt = sorted(freqList, key=lambda t: -t[1])[0][1]
+        nums = set([i for i, cnt in freqList if cnt == maxCnt])
+        for i in range(len(self.stack) - 1, -1, -1):
+            if self.stack[i] in nums:
+                ret = self.stack.pop(i)
+                self.freq[ret] -= 1
+                break
+        return ret
+
+
+#   https://leetcode.com/explore/featured/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3655
+#   runtime: 4212ms
+#   memory: 22.1MB, 91.24%
+class FreqStack:
+
+    def __init__(self):
+        self.stack, self.maxStack, self.freq = [], [], defaultdict(int)
+
+    def push(self, x):
+        self.stack.append(x)
+        self.freq[x] += 1
+        if 0 == len(self.maxStack) or self.freq[x] >= self.freq[self.maxStack[-1]]:
+            self.maxStack.append(x)
+        else:
+            self.maxStack.append(self.maxStack[-1])
+
+    def pop(self):
+        ret = self.maxStack.pop()
+        self.freq[ret] -= 1
+        for i in range(len(self.stack) - 1, -1, -1):
+            if self.stack[i] == ret:
+                self.stack.pop(i)
+                break
+        maxCnt = max(self.freq.values())
+        if 0 < len(self.maxStack) and self.freq[self.maxStack[-1]] <= maxCnt:
+            for i in range(len(self.stack) - 1, -1, -1):
+                if self.freq[self.stack[i]] == maxCnt:
+                    self.maxStack[-1] = self.stack[i]
+                    break
+        return ret
 
 
 # Your FreqStack object will be instantiated and called as such:
