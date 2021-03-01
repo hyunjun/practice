354132a58fee8fe2c57a46cc14ba6b1ced0b68a9
Hyunjun Chung <hyunjun.chung@gmail.com>
2021-03-01 17:13:42 +0800

    distribute candies https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3657

--- python/problem-O-of-n/distribute_candies.py
+++ python/problem-O-of-n/distribute_candies.py
@@ -4,11 +4,12 @@
 
 
 from collections import Counter
+from typing import List
 
 
 class Solution:
     #   30.66%
-    def distributeCandies(self, candies):
+    def distributeCandies0(self, candies):
         if candies is None or 0 == len(candies):
             return 0
         counter = Counter(candies)
@@ -17,6 +18,31 @@ class Solution:
         if halfLen <= keyLen:
             return halfLen
         return keyLen
+
+    #   https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3657
+    #   runtime: 908ms, 17.91%
+    #   memory: 16.8MB
+    def distributeCandies1(self, candyType: List[int]) -> int:
+        n, c = len(candyType) // 2, sorted(Counter(candyType).items(), key=lambda t: t[1])
+        if len(c) < n:
+            return len(c)
+        return n
+
+    #   runtime: 844ms, 32.32%
+    #   memory: 16MB, 94.46%
+    def distributeCandies2(self, candyType: List[int]) -> int:
+        n, cLen = len(candyType) // 2, len(Counter(candyType).keys())
+        if cLen < n:
+            return cLen
+        return n
+
+    #   runtime: 820ms, 42.94%
+    #   memory: 16.3MB, 66.24%
+    def distributeCandies(self, candyType: List[int]) -> int:
+        n, cLen = len(candyType) // 2, len(set(candyType))
+        if cLen < n:
+            return cLen
+        return n
 
 
 s = Solution()
@@ -27,6 +53,6 @@ data = [([1, 1, 1, 1, 1, 1], 1),
         ([1, 1, 2, 2, 3, 3], 3),
         ([1, 1, 2, 3], 2),
         ]
-for candies, expected in data:
+for candies, expect in data:
     real = s.distributeCandies(candies)
-    print('{}, expected {}, real {}, result {}'.format(candies, expected, real, expected == real))
+    print(f'{candies}, expect {expect}, real {real}, result {expect == real}')
