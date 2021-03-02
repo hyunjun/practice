6d66ddd3bcc47879d8a958c3eb5842df072dcbac
Jun <hyunjun.chung@gmail.com>
2021-03-02 17:40:26 +0800

    set mismatch https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3658

--- python/problem-O-of-n/set_mismatch.py
+++ python/problem-O-of-n/set_mismatch.py
@@ -3,9 +3,13 @@
 #   https://leetcode.com/problems/set-mismatch/solution
 
 
+from collections import Counter
+from typing import List
+
+
 class Solution:
     #   8.33%
-    def findErrorNums(self, nums):
+    def findErrorNums0(self, nums):
         if nums is None or 0 == len(nums):
             return []
         d, maxVal = {}, nums[0]
@@ -25,10 +29,24 @@ class Solution:
             missing = maxVal + 1
         return [twice, missing]
 
+    #   https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3658
+    #   runtime: 188ms, 82.20%
+    #   memory: 16MB, 31.95%
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        c, duplicated, missing = Counter(nums), -1, -1
+        for i in range(1, len(nums) + 1):
+            if i not in c:
+                missing = i
+            elif c[i] > 1:
+                duplicated = i
+        return [duplicated, missing]
+
 
 s = Solution()
 data = [([1, 2, 2, 4], [2, 3]),
-        ([1, 1], [1, 2])
+        ([1, 1], [1, 2]),
+        ([2, 2], [2, 1]),
+        ([3, 2, 2], [2, 1]),
         ]
 for nums, expected in data:
     real = s.findErrorNums(nums)
