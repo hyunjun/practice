faa8d5ef3a1d30dc73bcdb6c84b252e841616184
Jun <hyunjun.chung@gmail.com>
2021-03-03 17:12:09 +0800

    missing number https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3659

--- python/problem-math/missing_number.py
+++ python/problem-math/missing_number.py
@@ -1,11 +1,14 @@
 #   https://leetcode.com/problems/missing-number
-#   40.62%
 
 #   https://leetcode.com/problems/missing-number/solution
 
 
+from typing import List
+
+
 class Solution:
-    def missingNumber(self, nums):
+    #   40.62%
+    def missingNumber0(self, nums):
         if nums is None or 0 == len(nums):
             return None
         maxNum = nums[0]
@@ -20,10 +23,33 @@ class Solution:
                 return i
         return maxNum + 1
 
+    #   https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3659
+    #   runtime: 128ms, 81.32%
+    #   memory: 16.5MB
+    def missingNumber1(self, nums: List[int]) -> int:
+        maxNum = len(nums)
+        expectSet, realSet = set(i for i in range(maxNum)), set(nums)
+        if len(expectSet - realSet) == 0:
+            return maxNum
+        return list(expectSet - realSet)[0]
+
+    #   runtime: 124ms, 91.29%
+    #   memory: 16.5MB
+    def missingNumber(self, nums: List[int]) -> int:
+        maxNum = len(nums)
+        diffSet = set(i for i in range(maxNum)) - set(nums)
+        if len(diffSet) == 0:
+            return maxNum
+        return list(diffSet)[0]
 
 
 s = Solution()
-data = [([0], 1), ([2], 0), ([3, 0, 1], 2), ([9, 6, 4, 2, 3, 5, 7, 0, 1], 8)]
-for nums, expected in data:
+data = [([0], 1),
+        ([2], 0),
+        ([0, 1], 2),
+        ([3, 0, 1], 2),
+        ([9, 6, 4, 2, 3, 5, 7, 0, 1], 8),
+        ]
+for nums, expect in data:
     real = s.missingNumber(nums)
-    print('{}, expected {}, real {}, result {}'.format(nums, expected, real, expected == real))
+    print(f'{nums}, expect {expect}, real {real}, result {expect == real}')
