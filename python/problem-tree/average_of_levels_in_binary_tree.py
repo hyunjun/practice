68602cbc612fede22205755fb381c110bc09aaba
Jun <hyunjun.chung@gmail.com>
2021-03-05 17:12:43 +0800

    average of levels in binary tree https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3661

--- python/problem-tree/average_of_levels_in_binary_tree.py
+++ python/problem-tree/average_of_levels_in_binary_tree.py
@@ -4,11 +4,12 @@
 
 
 from TreeNode import TreeNode
+from typing import List
 
 
 class Solution:
     #   49.07%
-    def averageOfLevels(self, root):
+    def averageOfLevels0(self, root):
         if root is None:
             return []
 
@@ -28,6 +29,28 @@ class Solution:
         res.append(sum(vals) / len(vals))
         return res
 
+    #   https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3661
+    #   runtime: 52ms, 55.44%
+    #   memory: 16.6MB, 36.25%
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        q, levels, levelCnt = [(root, 0)], [], 0
+        while q:
+            n, d = q.pop(0)
+            if len(levels) == d:
+                if 0 < len(levels):
+                    levels[-1] /= levelCnt
+                    levelCnt = 0
+                levels.append(n.val)
+            else:
+                levels[-1] += n.val
+            levelCnt += 1
+            if n.left:
+                q.append((n.left, d + 1))
+            if n.right:
+                q.append((n.right, d + 1))
+        levels[-1] /= levelCnt
+        return levels
+
 
 s = Solution()
 root = TreeNode(3)
@@ -35,4 +58,8 @@ root.left = TreeNode(9)
 root.right = TreeNode(20)
 root.right.left = TreeNode(15)
 root.right.right = TreeNode(7)
-print(s.averageOfLevels(root))
+data = [(root, [3, 14.5, 11]),
+        ]
+for root, expect in data:
+    real = s.averageOfLevels(root)
+    print(f'{root} expect {expect} real {real} result {expect == real}')
