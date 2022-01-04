beb16c5d22c7f518d7c31031d481cd70cfc9e7ea
hyunjun.chung <hyunjun.chung@gmail.com>
2022-01-04 23:45:42 +0900

    Use context manager

--- python/test-zlib/download.py
+++ python/test-zlib/download.py
@@ -5,11 +5,10 @@ import requests
 @memory_profiler.profile
 def download(url, filepath):
     r = requests.get(url, stream=True)
-    f = open(filepath, 'wb')
-    for chunk in r.iter_content(chunk_size=1024):
-        if chunk:
-            f.write(chunk)
-    f.close()
+    with open(filepath, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
 
 
 url, filepath = 'http://speedtest.ftp.otenet.gr/files/test100Mb.db', '/tmp/test100Mb.db'
--- python/test-zlib/download_zlib.py
+++ python/test-zlib/download_zlib.py
@@ -7,21 +7,18 @@ import zlib
 def download(url, filepath):
     zipfilepath = filepath + '.zip'
     r = requests.get(url, stream=True)
-    f, compressor = open(zipfilepath, 'wb'), zlib.compressobj()
-    for chunk in r.iter_content(chunk_size=1024):
-        if chunk:
-            f.write(compressor.compress(chunk))
-    f.write(compressor.flush())
-    f.close()
+    compressor = zlib.compressobj()
+    with open(zipfilepath, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(compressor.compress(chunk))
+        f.write(compressor.flush())
 
-    f = open(zipfilepath, 'rb')
-    d = f.read()
-    f.close()
-
-    data = zlib.decompress(d)
-    f = open(filepath, 'wb')
-    f.write(data)
-    f.close()
+    with open(zipfilepath, 'rb') as rf:
+        d = rf.read()
+        data = zlib.decompress(d)
+        with open(filepath, 'wb') as f:
+            f.write(data)
 
 
 url, filepath = 'http://speedtest.ftp.otenet.gr/files/test100Mb.db', '/tmp/test100Mb.zlib.db'
