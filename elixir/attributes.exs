64a17963f577d19658f37bd75b6a0c6e4e929e5e
hyunjun.chung <hyunjun.chung@gmail.com>
2023-02-07 08:51:19 +0900

    p121

---
+++ elixir/attributes.exs
@@ -0,0 +1,9 @@
+# p121
+defmodule Example do
+  @author "Dave Thomas"
+  def get_author do
+    @author
+  end
+end
+
+IO.puts "Example was written by #{Example.get_author}"
---
+++ elixir/attributes1.exs
@@ -0,0 +1,9 @@
+# p121
+defmodule Example do
+  @attr "one"
+  def first, do: @attr
+  @attr "two"
+  def second, do: @attr
+end
+
+IO.puts "#{Example.second} #{Example.first}"
