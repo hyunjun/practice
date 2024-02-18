531325103a317bc3908724291d9babce2afba10a
hyunjun.chung <hyunjun.chung@gmail.com>
2024-02-18 21:25:21 +0900

    https://exercism.org/tracks/rust/exercises/sum-of-multiples

---
+++ rust/sum-of-multiples/src/lib.rs
@@ -0,0 +1,24 @@
+use std::collections::HashSet;
+
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut s = HashSet::new();
+    /*
+    for num in factors {
+        if num == &(0 as u32) {
+            continue;
+        }
+        for n in 1..limit {
+            if n % num == 0 {
+                s.insert(n);
+            }
+        }
+    }
+    */
+    for num in factors {
+        if num == &(0 as u32) {
+            continue;
+        }
+        s.extend((1..limit).filter(|n| n % num == 0));
+    }
+    s.into_iter().sum::<u32>()
+}
