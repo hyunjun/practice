78a64c89e0e8851d01112be43dd1575ec8e135d7
hyunjun.chung <hyunjun.chung@gmail.com>
2024-02-19 12:13:39 +0900

    https://exercism.org/tracks/rust/exercises/sum-of-multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,19 +1,18 @@
-use std::collections::HashSet;
+//use std::collections::HashSet;
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut s = HashSet::new();
     /*
-    for num in factors {
-        if num == &(0 as u32) {
-            continue;
-        }
-        for n in 1..limit {
-            if n % num == 0 {
-                s.insert(n);
-            }
-        }
-    }
-    */
+    let mut s = HashSet::new();
+    //for num in factors {
+    //    if num == &(0 as u32) {
+    //        continue;
+    //    }
+    //    for n in 1..limit {
+    //        if n % num == 0 {
+    //            s.insert(n);
+    //        }
+    //    }
+    //}
     for num in factors {
         if num == &(0 as u32) {
             continue;
@@ -21,4 +20,7 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
         s.extend((1..limit).filter(|n| n % num == 0));
     }
     s.into_iter().sum::<u32>()
+     */
+    //(1..limit).filter(|n| factors.iter().any(|f| &(0 as u32) < f && n % f == 0)).sum::<u32>()
+    (1..limit).filter(|n| factors.iter().filter(|f| &(0 as u32) < f).any(|f| n % f == 0)).sum::<u32>()
 }
