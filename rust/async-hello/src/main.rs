73f50c2330250bc91ba372a62a995cc1225b3aa6
hyunjun.chung <hyunjun.chung@gmail.com>
2024-02-25 14:14:26 +0900

    Basic example of tokio and future

--- rust/async-hello/src/main.rs
+++ rust/async-hello/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread::sleep;
 use std::time::Duration;
+use std::future::Future;
 
 #[tokio::main]
 async fn main() {
@@ -7,25 +8,31 @@ async fn main() {
 
     let h1 = tokio::spawn(async {
         let file1_contents = read_from_file1().await;
+        println!("{:?}", file1_contents);
     });
 
     let h2 = tokio::spawn(async {
         let file2_contents = read_from_file2().await;
+        println!("{:?}", file2_contents);
     });
 
     let _= tokio::join!(h1, h2);
 }
 
 // 파일 읽기를 시뮬레이션 하는 함수
-async fn read_from_file1() -> String {
-    sleep(Duration::new(4, 0));
-    println!("{:?}", "Processing file 1");
-    String::from("Hello, there from file 1")
+fn read_from_file1() -> impl Future<Output=String> {
+    async {
+        sleep(Duration::new(4, 0));
+        println!("{:?}", "Processing file 1");
+        String::from("Hello, there from file 1")
+    }
 }
 
 // 파일 읽기를 시뮬레이션 하는 함수
-async fn read_from_file2() -> String {
-    sleep(Duration::new(2, 0));
-    println!("{:?}", "Processing file 2");
-    String::from("Hello, there from file 2")
+fn read_from_file2() -> impl Future<Output=String> {
+    async {
+        sleep(Duration::new(2, 0));
+        println!("{:?}", "Processing file 2");
+        String::from("Hello, there from file 2")
+    }
 }
